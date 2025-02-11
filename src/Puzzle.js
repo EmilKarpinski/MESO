2c00e771ce60f33070b01d96199a3bd8f51174eb
EmilKarpinski <emil.karpinski.mac@gmail.com>
2025-02-11 15:55:26 -0500

    Reseting the base state. + added some comments.

--- src/Puzzle.js
+++ src/Puzzle.js
@@ -2,11 +2,11 @@
 export const Puzzle_Word_List = {
     "W1":["FANGS", "Pair of teeths"], 
     "W2":["ANIMAL", "Not a plant"],
-    // "W3":["APPLE", "One a day to keep the MDs away"],
-    // "W4":["FAKE", "Not real"],
-    // "W5":["VENOM", "Often mistaken for poison"],
+    "W3":["APPLE", "One a day to keep the MDs away"],
+    "W4":["FAKE", "Not real"],
+    "W5":["VENOM", "Often mistaken for poison"]
     // "W6":["SSS", "A hissing like sound"]
 };
 
 // Storing the central theme word
-export const Puzzle_ThemeWord = "SN";
+export const Puzzle_ThemeWord = "SNAKE";
