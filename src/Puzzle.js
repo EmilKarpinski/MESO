8eda2d7743ece0366682f7aabbe4c4a0e98feff8
EmilKarpinski <emil.karpinski.mac@gmail.com>
2025-03-06 10:47:45 -0500

    Fixed default clues

--- src/Puzzle.js
+++ src/Puzzle.js
@@ -1,10 +1,10 @@
 // Storing the puzzle information (words and clues for export)
 export const Puzzle_Word_List = {
-    "W1":["FANGS", "Pair of teeths"], 
-    "W2":["ANIMAL", "Not a plant"],
+    "W1":["FANGS", "Sharp pair of teeths"], 
+    "W2":["ANIMAL", "The kingdom of beasts"],
     "W3":["APPLE", "One a day to keep the MDs away"],
     "W4":["FAKE", "Not real"],
-    "W5":["VENOM", "Often mistaken for poison"]
+    "W5":["VENOM", "Poisonous substance from a bite"]
     // "W6":["SSS", "A hissing like sound"]
 };
 
