d629cfc94c61a2a2de63250f6b45c8bb9a785eee
EmilKarpinski <emil.karpinski.mac@gmail.com>
2025-03-28 14:25:09 -0400

    Scaffolding for future puzzles.

---
+++ src/PuzzleRepo/Puzzle_23.js
@@ -0,0 +1,15 @@
+// Storing the puzzle information (words and clues for export)
+export const Puzzle_Word_List = {
+    "W1":["", ""], 
+    "W2":["", ""],
+    "W3":["", ""],
+    "W4":["", ""],
+    "W5":["", ""],
+    "W6":["", ""]
+};
+
+// Storing the central theme word
+export const Puzzle_ThemeWord = "SUSHI";
+
+// Exporting the puzzle author
+export const Puzzle_Author = "Emil Karpinski"
