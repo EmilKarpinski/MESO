9b0745e905b1409695de6a8a1cc93ba349bf171d
EmilKarpinski <emil.karpinski.mac@gmail.com>
2025-03-28 13:38:41 -0400

    OG puzzle changes.

--- src/Puzzle_OG.js
+++ src/Puzzle_OG.js
@@ -1,12 +1,14 @@
 // Storing the puzzle information (words and clues for export)
 export const Puzzle_Word_List = {
-    "W1":["FANGS", "Sharp pair of teeths"], 
+    "W1":["FANGS", "Sharp pair of teeths like on a bat or vampire"], 
     "W2":["ANIMAL", "The kingdom of beasts"],
     "W3":["APPLE", "One a day to keep the MDs away"],
-    "W4":["FAKE", "Not real"],
-    "W5":["VENOM", "Poisonous substance from a bite"]
-    // "W6":["SSS", "A hissing like sound"]
+    "W4":["FAKE", "Not real or an imitation"],
+    "W5":["VENOM", "Toxic substance delivered during a bite"]
 };
 
 // Storing the central theme word
 export const Puzzle_ThemeWord = "SNAKE";
+
+// Exporting the puzzle author
+export const Puzzle_Author = "Emil Karpinski"
