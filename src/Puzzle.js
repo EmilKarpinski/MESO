8bcb4888240a431ec0b810b1eb143c46f6760aae
EmilKarpinski <emil.karpinski.mac@gmail.com>
2025-03-31 08:27:55 -0400

    Testing puzzle cachine with old puzzle.

--- src/Puzzle.js
+++ src/Puzzle.js
@@ -1,14 +1,15 @@
 // Storing the puzzle information (words and clues for export)
 export const Puzzle_Word_List = {
-    "W1":["FANGS", "Sharp pair of teeths like on a bat or vampire"], 
-    "W2":["ANIMAL", "The kingdom of beasts"],
-    "W3":["APPLE", "One a day to keep the MDs away"],
-    "W4":["FAKE", "Not real or an imitation"],
-    "W5":["VENOM", "Toxic substance delivered during a bite"]
+    "W1":["ECOLI", "Microbial workhorse (no punctuation)"], 
+    "W2":["MAMMOTH", "Bit bigger than a woolly mouse"],
+    "W3":["MULTIPLEX", "_________ all the things! (Do a bunch)"],
+    "W4":["CRISPR", "2020 Nobel Prize in Chemistry"],
+    "W5":["RECODE", "Remove a codon from an organism"],
+    "W6":["SYNTHESIS", "1MB+ plus around the corner? DNA _________"]
 };
 
 // Storing the central theme word
-export const Puzzle_ThemeWord = "SNAKE";
+export const Puzzle_ThemeWord = "CHURCH";
 
 // Exporting the puzzle author
 export const Puzzle_Author = "Emil Karpinski"
