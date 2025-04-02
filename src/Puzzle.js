628358124ea4c34871107766f3d33e45e4ce6fba
EmilKarpinski <emil.karpinski.mac@gmail.com>
2025-04-02 10:14:16 -0400

    Updates to today's puzzle.

--- src/Puzzle.js
+++ src/Puzzle.js
@@ -3,8 +3,8 @@ export const Puzzle_Word_List = {
     "W1":["FISH", "Most of a mermaid's friends (probably)"], 
     "W2":["WHITE", "The color of a 'great' predator's belly"],
     "W3":["JAWS", "Teeth in a blockbuster thriller"],
-    "W4":["PREDATOR", "Always hunting for a meal"],
-    "W5":["ATTACK", "Verb for an aggressive move"],
+    "W4":["PREDATOR", "Always hunting for a meal or Arnold Schwarzenegger"],
+    "W5":["ATTACK", "Verb for an aggressive move (not defend)"],
     "W6":["SEA", "The big blue, no land in sight"]
 };
 
