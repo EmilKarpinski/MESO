28913c393e9d63ed3722efa2383c6df2e1a92242
EmilKarpinski <emil.karpinski.mac@gmail.com>
2025-04-02 10:46:51 -0400

    Puzzle_30

---
+++ src/PuzzleRepo/Puzzle_30.js
@@ -0,0 +1,15 @@
+// Storing the puzzle information (words and clues for export)
+export const Puzzle_Word_List = {
+    "W1":["HUGO", ""], 
+    "W2":["OSCAR", ""],
+    "W3":["NEBULA", ""],
+    "W4":["NOBEL", ""],
+    "W5":["PULITZER", ""],
+    "W6":["AWARDS", ""]
+};
+
+// Storing the central theme word
+export const Puzzle_ThemeWord = "HONORS";
+
+// Exporting the puzzle author
+export const Puzzle_Author = "Beth Green and Emil Karpinski"
