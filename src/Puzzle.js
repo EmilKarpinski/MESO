53c3c7f204d693faa38bb5cd7a86e0dbcbeae961
EmilKarpinski <emil.karpinski.mac@gmail.com>
2025-04-04 09:27:42 -0400

    2025-04-04 Puzzle

--- src/Puzzle.js
+++ src/Puzzle.js
@@ -1,14 +1,15 @@
 // Storing the puzzle information (words and clues for export)
 export const Puzzle_Word_List = {
-    "W1":["FRESH", "Not canned or frozen"], 
-    "W2":["GRAPES", "Used to make wine"],
-    "W3":["COCONUT", "Tropical treat with a hard shell"],
-    "W4":["CITRUS", "Sour and zesty, perfect for juice (category)"],
-    "W5":["TREES", "If they could they wood leaf in the winter"]
+    "W1":["COURT", "Place where judges rule"], 
+    "W2":["RACKET", "A noisy commotion, or something in your hand"],
+    "W3":["GRAND", "_____ Slam"],
+    "W4":["POINT", "You might score these in a game"],
+    "W5":["SEMIFINAL", "The penultimate match"],
+    "W6":["SERVE", "To put a ball into play"]
 };
 
 // Storing the central theme word
-export const Puzzle_ThemeWord = "FRUIT";
+export const Puzzle_ThemeWord = "TENNIS";
 
 // Exporting the puzzle author
 export const Puzzle_Author = "Emil Karpinski"
