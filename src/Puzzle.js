394c72506905078e25d8224d158c296733cbcac6
EmilKarpinski <emil.karpinski.mac@gmail.com>
2025-04-07 08:58:54 -0400

    Today's puzzle and fixing bug in the html

--- src/Puzzle.js
+++ src/Puzzle.js
@@ -1,15 +1,14 @@
 // Storing the puzzle information (words and clues for export)
 export const Puzzle_Word_List = {
-    "W1":["COURT", "Place where judges rule"], 
-    "W2":["RACKET", "A noisy commotion, or something in your hand"],
-    "W3":["GRAND", "_____ Slam"],
-    "W4":["POINT", "You might score these in a game"],
-    "W5":["SEMIFINAL", "The penultimate match"],
-    "W6":["SERVE", "To put a ball into play"]
+    "W1":["NILE", "River that flows through Cairo"], 
+    "W2":["GLYPHS", "Ancient writing, heiro______"],
+    "W3":["PYRAMID", "Triangular or square geometry"],
+    "W4":["SPHINX", "A human-like cat"],
+    "W5":["TOMB", "Ancient resting place"]
 };
 
 // Storing the central theme word
-export const Puzzle_ThemeWord = "TENNIS";
+export const Puzzle_ThemeWord = "EGYPT";
 
 // Exporting the puzzle author
 export const Puzzle_Author = "Emil Karpinski"
