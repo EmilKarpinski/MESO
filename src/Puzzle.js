3f31e7856c819fb3c7b9a1185e80fc35b9155285
EmilKarpinski <emil.karpinski.mac@gmail.com>
2025-04-09 09:06:58 -0400

    Today's puzzle

--- src/Puzzle.js
+++ src/Puzzle.js
@@ -1,14 +1,14 @@
 // Storing the puzzle information (words and clues for export)
 export const Puzzle_Word_List = {
-    "W1":["CASTLE", "A home for royalty"], 
-    "W2":["KNIGHT", "In shining armor"],
-    "W3":["QUEEN", "Yas, slay _____"],
-    "W4":["BISHOP", "A church official with a big hat"],
-    "W5":["SQUARE", "The shape that's always right—literally"]
+    "W1":["CRIBBAGE", "Board, cards, and pegs"], 
+    "W2":["CATAN", "I've got wood for sheep. Anyone?"],
+    "W3":["MONOPOLY", "A hat, a car, and an iron try to buy some hotels"],
+    "W4":["CHESS", "Full of slanted clergy and confused horseman"],
+    "W5":["RISK", "Where “Take over Australia” is a totally reasonable goal"]
 };
 
 // Storing the central theme word
-export const Puzzle_ThemeWord = "CHESS";
+export const Puzzle_ThemeWord = "GAMES";
 
 // Exporting the puzzle author
 export const Puzzle_Author = "Emil Karpinski"
--- src/PuzzleRepo/UsedPuzzles/Puzzle_21.js
+++ src/PuzzleRepo/UsedPuzzles/Puzzle_21.js
@@ -1,6 +1,6 @@
 // Storing the puzzle information (words and clues for export)
 export const Puzzle_Word_List = {
-    "W1":["CRIBBAGE", "Board, card, and pegs"], 
+    "W1":["CRIBBAGE", "Board, cards, and pegs"], 
     "W2":["CATAN", "I've got wood for sheep. Anyone?"],
     "W3":["MONOPOLY", "A hat, a car, and an iron try to buy some hotels"],
     "W4":["CHESS", "Full of slanted clergy and confused horseman"],
