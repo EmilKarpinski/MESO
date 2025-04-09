5220f21db898ee436d694c7a83301ca511fcac0c
EmilKarpinski <emil.karpinski.mac@gmail.com>
2025-04-08 21:58:29 -0400

    Trying to implement a hidden input for mobile keyboard events.

--- src/index.js
+++ src/index.js
@@ -693,6 +693,8 @@ for (var i = 0; i < BoxList.length; i++){
 // Function that makes the clicked box the active box. 
 function BoxClicked(){
     if (this.classList.contains("right") || this.classList.contains("wrong")){
+
+        document.getElementById('input').focus(); 
         // Getting the position of the box clicked by parsing the ID string.
         // There's a max of 9 rows so I don't think the first vlaue should ever be more than 1 digit, wheras col can be multiple.
         let ClickRow = Number(this.id.substring(3,4));
