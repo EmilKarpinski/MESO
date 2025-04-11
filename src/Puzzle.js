6456e3dd52cc234c16f741c464a4a83740b34607
EmilKarpinski <emil.karpinski.mac@gmail.com>
2025-04-11 09:28:32 -0400

    Today's puzzle.

--- src/Puzzle.js
+++ src/Puzzle.js
@@ -1,16 +1,14 @@
 // Storing the puzzle information (words and clues for export)
 export const Puzzle_Word_List = {
-    "W1":["GIFT", "A surprise in a box, not a surprise visit"], 
-    "W2":["PLANT", "To place something suspiciously or on someone"],
-    "W3":["BLOOM", "What a garden is full of in spring"],
-    "W4":["WILD", "Not tame or domesticated"],
-    "W5":["HONEY", "Sweet, sticky treat from bees"],
-    "W6":["COLORS", "e.g. Red, white, and blue"],
-    "W7":["ROSE", "A floral symbol of love"]
+    "W1":["SUBMARINE", "The ultimate hide-and-seek champion, but with more torpedoes"], 
+    "W2":["CANOE", "A rocky DIY paddle-powered vessel"],
+    "W3":["NAVY", "They get to wear little white hats instead of camouflage"],
+    "W4":["YACHT", "A floating status symbol"],
+    "W5":["FISHING", "Might be to relax, might be for sushi"]
 };
 
 // Storing the central theme word
-export const Puzzle_ThemeWord = "FLOWERS";
+export const Puzzle_ThemeWord = "BOATS";
 
 // Exporting the puzzle author
 export const Puzzle_Author = "Emil Karpinski"
