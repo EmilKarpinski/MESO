0ea9f6240d3a18b4cbd4e2b383ab8615fa20beef
EmilKarpinski <emil.karpinski.mac@gmail.com>
2025-04-14 08:41:20 -0400

    Today's Puzzle (2025-04-14)

--- src/Puzzle.js
+++ src/Puzzle.js
@@ -1,14 +1,14 @@
 // Storing the puzzle information (words and clues for export)
 export const Puzzle_Word_List = {
-    "W1":["SUBMARINE", "The ultimate hide-and-seek champion, but with more torpedoes"], 
-    "W2":["CANOE", "A rocky DIY paddle-powered vessel"],
-    "W3":["NAVY", "They get to wear little white hats instead of camouflage"],
-    "W4":["YACHT", "A floating status symbol"],
-    "W5":["FISHING", "Might be to relax, might be for sushi"]
+    "W1":["BLACK", "A misfortunate feline"], 
+    "W2":["COFFEE", "Get's you up and keeps you going"],
+    "W3":["CHICKPEA", "Also sometimes refered to as a Garbanzo"],
+    "W4":["KIDNEY", "The thing you've got two of, but can live with just one"],
+    "W5":["SOY", "Often made into milk or tofu"]
 };
 
 // Storing the central theme word
-export const Puzzle_ThemeWord = "BOATS";
+export const Puzzle_ThemeWord = "BEANS";
 
 // Exporting the puzzle author
 export const Puzzle_Author = "Emil Karpinski"
