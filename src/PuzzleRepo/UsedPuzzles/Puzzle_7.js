d8285d567976fd867e1b08ccc23e4890b56f1041
EmilKarpinski <emil.karpinski.mac@gmail.com>
2025-04-15 10:08:49 -0400

    Forgot to move the puzzle to the used ones

--- src/PuzzleRepo/UsedPuzzles/Puzzle_7.js
+++ src/PuzzleRepo/UsedPuzzles/Puzzle_7.js
@@ -2,8 +2,8 @@
 export const Puzzle_Word_List = {
     "W1":["WRITE", "We used to _____ letters. Now we _____ emails"], 
     "W2":["JOB", "Something you complain about, but still show up to every day"],
-    "W3":["SKILLS", "Per ND: 'Girls only want boyfriends who have great ______'"],
-    "W4":["BREAK", "A pause in your busy day"]
+    "W3":["BREAK", "A pause in your busy day"],
+    "W4":["SKILLS", "Per ND: 'Girls only want boyfriends who have great ______'"]
 };
 
 // Storing the central theme word
