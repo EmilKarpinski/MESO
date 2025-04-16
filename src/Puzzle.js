e99d77700b7bbf378e1bfd4a0648bcb0497ae93b
EmilKarpinski <emil.karpinski.mac@gmail.com>
2025-04-16 08:59:10 -0400

    Today's Puzzle (2025-04-16)

--- src/Puzzle.js
+++ src/Puzzle.js
@@ -1,13 +1,13 @@
 // Storing the puzzle information (words and clues for export)
 export const Puzzle_Word_List = {
-    "W1":["WRITE", "We used to _____ letters. Now we _____ emails"], 
-    "W2":["JOB", "Something you complain about, but still show up to every day"],
-    "W3":["BREAK", "A pause in your busy day"],
-    "W4":["SKILLS", "Per ND: 'Girls only want boyfriends who have great ______'"]
+    "W1":["MUSCLE", "Brawn, not brains"], 
+    "W2":["RACE", "Fast competition"],
+    "W3":["GARAGE", "Home for your vehicle"],
+    "W4":["SEATBELT", "Buckle up!"]
 };
 
 // Storing the central theme word
-export const Puzzle_ThemeWord = "WORK";
+export const Puzzle_ThemeWord = "CARS";
 
 // Exporting the puzzle author
 export const Puzzle_Author = "Emil Karpinski"
