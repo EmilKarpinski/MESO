0c52096758bac6a3bb79a3803b9314db8e672684
EmilKarpinski <emil.karpinski.mac@gmail.com>
2025-04-21 09:03:23 -0400

    Today's Puzzle (2025-04-21)

--- src/Puzzle.js
+++ src/Puzzle.js
@@ -1,17 +1,15 @@
 // Storing the puzzle information (words and clues for export)
 export const Puzzle_Word_List = {
-    "W1":["LIZARD", "A reptile with a talent for sunbathing and tail regeneration"], 
-    "W2":["BIRD", "A big one lives on Sesame street"],
-    "W3":["EXTINCT", "Not alive, not coming back, but still very interesting animals"],
-    "W4":["RAPTOR", "E.g. hawks, eagles, falcons"],
-    "W5":["FOSSIL", "A rock that used to be something alive"],
-    "W6":["ANCIENT", "There's old then there's _______"],
-    "W7":["JURASSIC", "A famous fictional park that didn't quite go as planned"],
-    "W8":["TREX", "Tyrant lizard king"]
+    "W1":["EGGS", "Cracked open in the morning with some toast"], 
+    "W2":["LAMB", "Wool-covered cutie"],
+    "W3":["SPRING", "Bouncy season"],
+    "W4":["HUNT", "To chase or seek"],
+    "W5":["BUNNIES", "Cute hoppers with long ears"],
+    "W6":["BRUNCH", "A late morning feast"]
 };
 
 // Storing the central theme word
-export const Puzzle_ThemeWord = "DINOSAUR";
+export const Puzzle_ThemeWord = "EASTER";
 
 // Exporting the puzzle author
 export const Puzzle_Author = "Emil Karpinski"
