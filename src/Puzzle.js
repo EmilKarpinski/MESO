dcd3e480774fdd7585a3133f3f059bde23049ff2
EmilKarpinski <emil.karpinski.mac@gmail.com>
2025-04-24 09:17:27 -0400

    Today's puzzle (2025-04-23)

--- src/Puzzle.js
+++ src/Puzzle.js
@@ -1,14 +1,15 @@
 // Storing the puzzle information (words and clues for export)
 export const Puzzle_Word_List = {
-    "W1":["SEAWEED", "Ocean's salad"], 
-    "W2":["TUNA", "Found in a salad or a can"],
-    "W3":["JAPANESE", "Ramen and kimonos are this"],
-    "W4":["SASHIMI", "I prefer my fish 'au naturel'"],
-    "W5":["GINGER", "Spicy root in a lot of Asian cuisine"]
+    "W1":["BASEBALL", "Take me out to the ball game"], 
+    "W2":["POLO", "Played either in water or on horseback"],
+    "W3":["FOOTBALL", "Sunday pigskin"],
+    "W4":["TRACK", "Racing circuit"],
+    "W5":["SKATING", "How hockey players get around the ice"],
+    "W6":["WRESTLING", "e.g. WWE, Greco-Roman, Lucha libre"]
 };
 
 // Storing the central theme word
-export const Puzzle_ThemeWord = "SUSHI";
+export const Puzzle_ThemeWord = "SPORTS";
 
 // Exporting the puzzle author
 export const Puzzle_Author = "Emil Karpinski"
