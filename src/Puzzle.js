de226b5bec27e9238a8d4db36ea8da3e18bf5894
EmilKarpinski <emil.karpinski.mac@gmail.com>
2025-04-28 11:31:52 -0400

    2025-04-28 Puzzle

--- src/Puzzle.js
+++ src/Puzzle.js
@@ -1,15 +1,14 @@
 // Storing the puzzle information (words and clues for export)
 export const Puzzle_Word_List = {
-    "W1":["DECIPHER", "To unravel or solve"], 
-    "W2":["SUDOKU", "Like a spreadsheet, but for fun"],
-    "W3":["MAZE", "Complex network of paths"],
-    "W4":["BIZARRE", "Strange, unusual, or confusing (Adj)"],
-    "W5":["PROBLEM", "A challenge to figure out"],
-    "W6":["GAME", "A sport or boardgame"]
+    "W1":["PISA", "A tilting tower"], 
+    "W2":["PASTA", "Carbs in many shapes"],
+    "W3":["PIZZA", "Favorite pie of adolescent turtles"],
+    "W4":["POLENTA", "Yellow and creamy cornmeal dish"],
+    "W5":["PAPACY", "This office runs the Catholic church"]
 };
 
 // Storing the central theme word
-export const Puzzle_ThemeWord = "PUZZLE";
+export const Puzzle_ThemeWord = "ITALY";
 
 // Exporting the puzzle author
 export const Puzzle_Author = "Emil Karpinski"
