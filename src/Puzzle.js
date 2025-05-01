fe5701800104369256a20865446988211748a091
EmilKarpinski <emil.karpinski.mac@gmail.com>
2025-05-01 09:13:33 -0400

    Puzzle 2025-05-01

--- src/Puzzle.js
+++ src/Puzzle.js
@@ -1,14 +1,14 @@
 // Storing the puzzle information (words and clues for export)
 export const Puzzle_Word_List = {
-    "W1":["MUSHROOM", "Buttons for a 'Fun-gi'"], 
-    "W2":["JAPANESE", "Culture associated with sushi and soba"],
-    "W3":["PLUMBER", "Unclogs your worst problems"],
-    "W4":["ITALIAN", "Culture associated with pizza and pasta"],
-    "W5":["NINTENDO", "Wii switch'd the cube, then switch'd it again"]
+    "W1":["FLIES", "Piggy, Simon, and a conch. Lord of the _____"], 
+    "W2":["KERMIT", "Famous street-wise puppet"],
+    "W3":["HOP", "A little jump"],
+    "W4":["TONGUE", "Needed for talking and tasting"],
+    "W5":["SWAMP", "Might contain, lizards, witches, and rodents of unusual size"]
 };
 
 // Storing the central theme word
-export const Puzzle_ThemeWord = "MARIO";
+export const Puzzle_ThemeWord = "FROGS";
 
 // Exporting the puzzle author
 export const Puzzle_Author = "Emil Karpinski"
