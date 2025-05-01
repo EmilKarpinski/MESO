db7b9e7b13f70054cae77f185366f2204225ea57
EmilKarpinski <emil.karpinski.mac@gmail.com>
2025-05-01 09:14:37 -0400

    Birthday puzzle template

---
+++ src/PuzzleRepo/Puzzle_Birthday.js
@@ -0,0 +1,17 @@
+// Storing the puzzle information (words and clues for export)
+export const Puzzle_Word_List = {
+    "W1":["", ""], 
+    "W2":["", ""],
+    "W3":["", ""],
+    "W4":["", ""],
+    "W5":["", ""],
+    "W6":["", ""],
+    "W7":["", ""],
+    "W8":["", ""],
+};
+
+// Storing the central theme word
+export const Puzzle_ThemeWord = "BIRTHDAY";
+
+// Exporting the puzzle author
+export const Puzzle_Author = "Emil Karpinski"
