b9221b6e764034cbea24c273730ca0a8ebc1e56b
EmilKarpinski <emil.karpinski.mac@gmail.com>
2025-05-02 09:05:44 -0400

    puzzle 2025-05-02

--- src/Puzzle.js
+++ src/Puzzle.js
@@ -1,14 +1,15 @@
 // Storing the puzzle information (words and clues for export)
 export const Puzzle_Word_List = {
-    "W1":["FLIES", "Piggy, Simon, and a conch. Lord of the _____"], 
-    "W2":["KERMIT", "Famous street-wise puppet"],
-    "W3":["HOP", "A little jump"],
-    "W4":["TONGUE", "Needed for talking and tasting"],
-    "W5":["SWAMP", "Might contain, lizards, witches, and rodents of unusual size"]
+    "W1":["BREAD", "Like toast, but fresh"], 
+    "W2":["CAKE", "Marie Antoinette's favorite dessert"],
+    "W3":["COOKIES", "A blue monster's favorite snack"],
+    "W4":["PASTRIES", "e.g. croissants, danishes, eclairs"],
+    "W5":["CINNAMON", "Titular ingredient in tasty buns"],
+    "W6":["FROSTING", "What you slather on cupcakes, not toast "]
 };
 
 // Storing the central theme word
-export const Puzzle_ThemeWord = "FROGS";
+export const Puzzle_ThemeWord = "BAKING";
 
 // Exporting the puzzle author
 export const Puzzle_Author = "Emil Karpinski"
