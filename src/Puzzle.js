81dd0fd412b9be115cc1d0701da176b86e2e1d85
EmilKarpinski <emil.karpinski.mac@gmail.com>
2025-05-05 08:58:54 -0400

    Today Puzzle (2025-05-05)

--- src/Puzzle.js
+++ src/Puzzle.js
@@ -1,15 +1,17 @@
 // Storing the puzzle information (words and clues for export)
 export const Puzzle_Word_List = {
-    "W1":["BREAD", "Like toast, but fresh"], 
-    "W2":["CAKE", "Marie Antoinette's favorite dessert"],
-    "W3":["COOKIES", "A blue monster's favorite snack"],
-    "W4":["PASTRIES", "e.g. croissants, danishes, eclairs"],
-    "W5":["CINNAMON", "Titular ingredient in tasty buns"],
-    "W6":["FROSTING", "What you slather on cupcakes, not toast "]
+    "W1":["REBELS", "Revolutionaries from another point of view."], 
+    "W2":["BOUNTY", "A prize for the capture of criminals."],
+    "W3":["ANDROID", "Smartphone OS or a synthetic being."],
+    "W4":["FORCE", "What you get when you accelerate some mass."],
+    "W5":["HOLLYWOOD", "Blockbuster borough."],
+    "W6":["SPACE", "NASA's playground."],
+    "W7":["EMPEROR","Julius Ceaser was the first Roman this."],
+    "W8":["SABER","The category of curved swords which includes those favored by pirates or cavalrymen."]
 };
 
 // Storing the central theme word
-export const Puzzle_ThemeWord = "BAKING";
+export const Puzzle_ThemeWord = "STARWARS";
 
 // Exporting the puzzle author
 export const Puzzle_Author = "Emil Karpinski"
