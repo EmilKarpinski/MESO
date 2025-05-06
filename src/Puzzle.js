2773a2c47bf92b64031c40fcd18292f53e1dc358
EmilKarpinski <emil.karpinski.mac@gmail.com>
2025-05-06 11:10:42 -0400

    Typo

--- src/Puzzle.js
+++ src/Puzzle.js
@@ -1,7 +1,7 @@
 // Storing the puzzle information (words and clues for export)
 export const Puzzle_Word_List = {
     "W1":["DEBT", "What you have when you spend money you didn't."], 
-    "W2":["LOAN", "Requires and lender and a borrower."],
+    "W2":["LOAN", "Requires a lender and a borrower."],
     "W3":["INTEREST", "Financial charge or held curiosity."],
     "W4":["LOCK", "A strand of hair or security measure."],
     "W5":["DEPOSIT", "An initial payment to secure something."]
