6348306ee74a52202c8611f0f7f03c62438e0e9b
EmilKarpinski <emil.karpinski.mac@gmail.com>
2025-05-07 08:53:06 -0400

    Today's Puzzle (2025-05-07)

--- src/Puzzle.js
+++ src/Puzzle.js
@@ -1,14 +1,13 @@
 // Storing the puzzle information (words and clues for export)
 export const Puzzle_Word_List = {
-    "W1":["DEBT", "What you have when you spend money you didn't."], 
-    "W2":["LOAN", "Requires a lender and a borrower."],
-    "W3":["INTEREST", "Financial charge or held curiosity."],
-    "W4":["LOCK", "A strand of hair or security measure."],
-    "W5":["DEPOSIT", "An initial payment to secure something."]
+    "W1":["NUMBER", "A countable thing."], 
+    "W2":["CALCULUS", "Advanced math that's integral for higher learning."],
+    "W3":["GEOMETRY", "Triangles and shapes all the way down."],
+    "W4":["HOMEWORK", "Lessons that follow you home."]
 };
 
 // Storing the central theme word
-export const Puzzle_ThemeWord = "BANKS";
+export const Puzzle_ThemeWord = "MATH";
 
 // Exporting the puzzle author
 export const Puzzle_Author = "Emil Karpinski"
