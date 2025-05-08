1bbbf1b265f0f211ced29733de67ea40a02d0c69
EmilKarpinski <emil.karpinski.mac@gmail.com>
2025-05-08 09:16:20 -0400

    Today's Puzzle (2025-05-08)

--- src/Puzzle.js
+++ src/Puzzle.js
@@ -1,13 +1,17 @@
 // Storing the puzzle information (words and clues for export)
 export const Puzzle_Word_List = {
-    "W1":["NUMBER", "A countable thing."], 
-    "W2":["CALCULUS", "Advanced math that's integral for higher learning."],
-    "W3":["GEOMETRY", "Triangles and shapes all the way down."],
-    "W4":["HOMEWORK", "Lessons that follow you home."]
+    "W1":["BRUSH", "Tool for teeth or hair."], 
+    "W2":["SOAP", "Hygienic bar or liquid."],
+    "W3":["BIDET", "A superior spraying alternative to toilet paper."],
+    "W4":["SHOWER", "A place or action of cleaning."],
+    "W5":["MIRROR", "Narcissus's friend."],
+    "W6":["TOILET", "A porcelain throne."],
+    "W7":["TOWEL", "Used to dry off or sit on at the beach."],
+    "W8":["SHAMPOO", "Sudsy scalp solution."]
 };
 
 // Storing the central theme word
-export const Puzzle_ThemeWord = "MATH";
+export const Puzzle_ThemeWord = "BATHROOM";
 
 // Exporting the puzzle author
 export const Puzzle_Author = "Emil Karpinski"
