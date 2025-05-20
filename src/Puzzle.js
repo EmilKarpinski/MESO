2d440640e9c3f3a72fe7d5f22380e26e27946280
EmilKarpinski <emil.karpinski.mac@gmail.com>
2025-05-20 09:05:09 -0400

    Today's puzzle and a small change to the socials button

--- src/Puzzle.js
+++ src/Puzzle.js
@@ -1,17 +1,18 @@
 // Storing the puzzle information (words and clues for export)
 export const Puzzle_Word_List = {
-    "W1":["CANS", "Used for soup, beer, or beans."], 
-    "W2":["COKE", "Black or white. Drank or snorted respectively."],
-    "W3":["DIET", "Low-calorie plan."],
-    "W4":["GINGERALE", "Root-freshing beverage (two words)."],
-    "W5":["SUGAR", "Granulated or powdered."]
+    "W1":["BLACK", "Night, bats, and bad moods."], 
+    "W2":["CAPE", "Billowing super symbol."],
+    "W3":["VIGILANTE", "Justice seeker or lawless criminal depending on the POV."],
+    "W4":["CRIME", "Disallowed by law."],
+    "W5":["GADGETS", "Supplied by Q to secret agents."],
+    "W6":["ROBIN", "Red chested flyer."]
 };
 
 // Storing the central theme word
-export const Puzzle_ThemeWord = "SODAS";
+export const Puzzle_ThemeWord = "BATMAN";
 
 // Storing the previous meso for streak purposes
-export const Previous_Meso = "CHEMISTRY";
+export const Previous_Meso = "SODAS";
 
 // Exporting the puzzle author
 export const Puzzle_Author = "Emil Karpinski"
--- src/PuzzleRepo/UsedPuzzles/Puzzle_45.js
+++ src/PuzzleRepo/UsedPuzzles/Puzzle_45.js
@@ -12,7 +12,7 @@ export const Puzzle_Word_List = {
 export const Puzzle_ThemeWord = "BATMAN";
 
 // Storing the previous meso for streak purposes
-export const Previous_Meso = "";
+export const Previous_Meso = "SODAS";
 
 // Exporting the puzzle author
 export const Puzzle_Author = "Emil Karpinski"
