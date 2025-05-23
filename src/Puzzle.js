9ca0a5a1cf0d223f9f77c6a6e82adaf31855189c
Emil Karpinski <102920028+EmilKarpinski@users.noreply.github.com>
2025-05-23 09:31:08 -0400

    Cellphone commit

--- src/Puzzle.js
+++ src/Puzzle.js
@@ -1,18 +1,19 @@
 // Storing the puzzle information (words and clues for export)
 export const Puzzle_Word_List = {
-    "W1":["MATTRESS", "Springboard for your dreams."], 
-    "W2":["BOXES", "Cardboard cubes."],
-    "W3":["HEAVY", "Not light, emotionally or physically."],
-    "W4":["SHIPPING", "To move things by boat or truck."],
-    "W5":["FRIENDS", "Supportive chosen family."],
-    "W6":["FRAGILE", "Easily broken."]
+    "W1":["PURITY", "Undiluted virtue."], 
+    "W2":["HORN", "Found in traffic or the brass section."],
+    "W3":["WHITE", "Color of snow or some lies."],
+    "W4":["MAGICAL", "Elves and other fae."],
+    "W5":["FOREST", "You can't see the trees for this."],
+    "W6":["HORSE", "Knight's ride."],
+    "W7":["VIRGIN", "Almost maiden Madonna album."]
 };
 
 // Storing the central theme word
-export const Puzzle_ThemeWord = "MOVING";
+export const Puzzle_ThemeWord = "UNICORN";
 
 // Storing the previous meso for streak purposes
-export const Previous_Meso = "BIRTHDAY";
+export const Previous_Meso = "MOVING";
 
 // Exporting the puzzle author
-export const Puzzle_Author = "Emil Karpinski"
+export const Puzzle_Author = "Emil Karpinski";
