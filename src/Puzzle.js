e10559edf52e49bba5e6fda24e805b17ff1b64a0
EmilKarpinski <emil.karpinski.mac@gmail.com>
2025-05-26 09:47:20 -0400

    Today's Puzzles

--- src/Puzzle.js
+++ src/Puzzle.js
@@ -1,19 +1,17 @@
 // Storing the puzzle information (words and clues for export)
 export const Puzzle_Word_List = {
-    "W1":["PURITY", "Undiluted virtue."], 
-    "W2":["HORN", "Found in traffic or the brass section."],
-    "W3":["WHITE", "Color of snow or some lies."],
-    "W4":["MAGICAL", "Elves and other fae."],
-    "W5":["FOREST", "You can't see the trees for this."],
-    "W6":["HORSE", "Knight's ride."],
-    "W7":["VIRGIN", "Almost maiden Madonna album."]
+    "W1":["STEAM", "Hisses from a kettle."], 
+    "W2":["TRACK", "A path or lane."],
+    "W3":["SUBWAY", "Tunnel transit or sandwiches."],
+    "W4":["RAIL", "Can be used to slide down the stairs or grind with a skateboard."],
+    "W5":["CROSSING", "Where two roads meet."]
 };
 
 // Storing the central theme word
-export const Puzzle_ThemeWord = "UNICORN";
+export const Puzzle_ThemeWord = "TRAIN";
 
 // Storing the previous meso for streak purposes
-export const Previous_Meso = "MOVING";
+export const Previous_Meso = "UNICORN";
 
 // Exporting the puzzle author
-export const Puzzle_Author = "Emil Karpinski";
+export const Puzzle_Author = "Emil Karpinski"
--- src/PuzzleRepo/UsedPuzzles/Puzzle_41.js
+++ src/PuzzleRepo/UsedPuzzles/Puzzle_41.js
@@ -1,6 +1,6 @@
 // Storing the puzzle information (words and clues for export)
 export const Puzzle_Word_List = {
-    "W1":["STEAM", "Hissess from a kettle."], 
+    "W1":["STEAM", "Hisses from a kettle."], 
     "W2":["TRACK", "A path or lane."],
     "W3":["SUBWAY", "Tunnel transit or sandwiches."],
     "W4":["RAIL", "Can be used to slide down the stairs or grind with a skateboard."],
@@ -11,7 +11,7 @@ export const Puzzle_Word_List = {
 export const Puzzle_ThemeWord = "TRAIN";
 
 // Storing the previous meso for streak purposes
-export const Previous_Meso = "";
+export const Previous_Meso = "UNICORN";
 
 // Exporting the puzzle author
 export const Puzzle_Author = "Emil Karpinski"
