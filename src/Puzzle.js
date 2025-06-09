c09bfc7100075e81f9aea2ecb05efa8de9cd09e1
EmilKarpinski <emil.karpinski.mac@gmail.com>
2025-06-09 09:46:36 -0400

    2025-06-06 Puzzle

--- src/Puzzle.js
+++ src/Puzzle.js
@@ -1,17 +1,16 @@
 // Storing the puzzle information (words and clues for export)
 export const Puzzle_Word_List = {
-    "W1":["TRIUMPH", "A significant victory."], 
-    "W2":["ROMANCE", "'Novel' kind of love."],
-    "W3":["TOWER", "Tall architecture."],
-    "W4":["LIGHTS", "Signal stop or go."],
-    "W5":["CROISSANT", "Layered edible crescent."]
+    "W1":["THYME", "Fragrant plant that clocks in?"], 
+    "W2":["ROSEMARY", "Good for cooking, not great for professing love or marriage."],
+    "W3":["PARSLEY", "Curly or flat garnish."],
+    "W4":["BASIL", "Key ingredient in pesto."]
 };
 
 // Storing the central theme word
-export const Puzzle_ThemeWord = "PARIS";
+export const Puzzle_ThemeWord = "HERB";
 
 // Storing the previous meso for streak purposes
-export const Previous_Meso = "VAMPIRE";
+export const Previous_Meso = "PARIS";
 
 // Exporting the puzzle author
 export const Puzzle_Author = "Emil Karpinski";
