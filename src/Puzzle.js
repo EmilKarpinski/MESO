56a243d53d4099d0814f8ada7de8cbcd9effaba5
EmilKarpinski <emil.karpinski.mac@gmail.com>
2025-06-16 09:39:46 -0400

    Launch Puzzle

--- src/Puzzle.js
+++ src/Puzzle.js
@@ -1,18 +1,16 @@
 // Storing the puzzle information (words and clues for export)
 export const Puzzle_Word_List = {
-    "W1":["COSTUME", "Like a uniform but fun to wear."], 
-    "W2":["SHIFT", "Alter position or gear."],
-    "W3":["VARIES", "Not always the same."],
-    "W4":["COINS", "Pennies, nickels, and dimes."],
-    "W5":["UPGRADE", "Replace with something better."],
-    "W6":["TRANSFER", "Move from one place to another."]
+    "W1":["STORM", "Tempest or tantrum."], 
+    "W2":["WINTER", "Fourth quarter freeze."],
+    "W3":["COLD", "Short on warmth, or emotion."],
+    "W4":["WHITE", "Shade of surrender."]
 };
 
 // Storing the central theme word
-export const Puzzle_ThemeWord = "CHANGE";
+export const Puzzle_ThemeWord = "SNOW";
 
 // Storing the previous meso for streak purposes
-export const Previous_Meso = "STAMP";
+export const Previous_Meso = "CHANGE";
 
 // Exporting the puzzle author
 export const Puzzle_Author = "Emil Karpinski";
