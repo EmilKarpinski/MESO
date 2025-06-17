466622dcdfe69e712c004dd43322b801fd463bae
EmilKarpinski <emil.karpinski.mac@gmail.com>
2025-06-17 09:20:21 -0400

    2025-06-17

--- src/Puzzle.js
+++ src/Puzzle.js
@@ -1,16 +1,17 @@
 // Storing the puzzle information (words and clues for export)
 export const Puzzle_Word_List = {
-    "W1":["STORM", "Tempest or tantrum."], 
-    "W2":["WINTER", "Fourth quarter freeze."],
-    "W3":["COLD", "Short on warmth, or emotion."],
-    "W4":["WHITE", "Shade of surrender."]
+    "W1":["LIGHT", "Opposite of dark and dense."], 
+    "W2":["BEAM", "Supports or to radiates happiness."],
+    "W3":["SHOW", "Put on display."],
+    "W4":["RED", "Favored hood color of forest girls who love their grandmothers."],
+    "W5":["CONCERT", "Live music performance. "]
 };
 
 // Storing the central theme word
-export const Puzzle_ThemeWord = "SNOW";
+export const Puzzle_ThemeWord = "LASER";
 
 // Storing the previous meso for streak purposes
-export const Previous_Meso = "CHANGE";
+export const Previous_Meso = "SNOW";
 
 // Exporting the puzzle author
 export const Puzzle_Author = "Emil Karpinski";
--- src/PuzzleRepo/UsedPuzzles/Puzzle_57.js
+++ src/PuzzleRepo/UsedPuzzles/Puzzle_57.js
@@ -1,9 +1,9 @@
 // Storing the puzzle information (words and clues for export)
 export const Puzzle_Word_List = {
     "W1":["LIGHT", "Opposite of dark and dense."], 
-    "W2":["BEAM", "Supports or to radiate happiness."],
+    "W2":["BEAM", "Supports or to radiates happiness."],
     "W3":["SHOW", "Put on display."],
-    "W4":["RED", "Favored hood color of forest girls."],
+    "W4":["RED", "Favored hood color of forest girls who love their grandmothers."],
     "W5":["CONCERT", "Live music performance. "]
 };
 
@@ -11,7 +11,7 @@ export const Puzzle_Word_List = {
 export const Puzzle_ThemeWord = "LASER";
 
 // Storing the previous meso for streak purposes
-export const Previous_Meso = "";
+export const Previous_Meso = "SNOW";
 
 // Exporting the puzzle author
 export const Puzzle_Author = "Emil Karpinski";
