b45f5f02a750f8fb8676344045a0e0d3ee2195dd
EmilKarpinski <emil.karpinski.mac@gmail.com>
2025-06-18 09:41:40 -0400

    2025-06-18

--- src/Puzzle.js
+++ src/Puzzle.js
@@ -1,17 +1,16 @@
 // Storing the puzzle information (words and clues for export)
 export const Puzzle_Word_List = {
-    "W1":["LIGHT", "Opposite of dark and dense."], 
-    "W2":["BEAM", "Supports or to radiates happiness."],
-    "W3":["SHOW", "Put on display."],
-    "W4":["RED", "Favored hood color of forest girls who love their grandmothers."],
-    "W5":["CONCERT", "Live music performance. "]
+    "W1":["WANT", "Desire or to lack something."], 
+    "W2":["GENIE", "Aladdin's pottery pal."],
+    "W3":["STAR", "Celestial body or celeb."],
+    "W4":["HOPE", "Faith or optimism."]
 };
 
 // Storing the central theme word
-export const Puzzle_ThemeWord = "LASER";
+export const Puzzle_ThemeWord = "WISH";
 
 // Storing the previous meso for streak purposes
-export const Previous_Meso = "SNOW";
+export const Previous_Meso = "LASER";
 
 // Exporting the puzzle author
 export const Puzzle_Author = "Emil Karpinski";
--- src/PuzzleRepo/UsedPuzzles/Puzzle_WISH.js
+++ src/PuzzleRepo/UsedPuzzles/Puzzle_WISH.js
@@ -10,7 +10,7 @@ export const Puzzle_Word_List = {
 export const Puzzle_ThemeWord = "WISH";
 
 // Storing the previous meso for streak purposes
-export const Previous_Meso = "";
+export const Previous_Meso = "LASER";
 
 // Exporting the puzzle author
 export const Puzzle_Author = "Emil Karpinski";
