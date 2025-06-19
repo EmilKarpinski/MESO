4b3b697a8764f28b138fb5566121ba3c3a825bbd
EmilKarpinski <emil.karpinski.mac@gmail.com>
2025-06-19 09:30:25 -0400

    Today and tomorrow's puzzle

--- src/Puzzle.js
+++ src/Puzzle.js
@@ -1,16 +1,17 @@
 // Storing the puzzle information (words and clues for export)
 export const Puzzle_Word_List = {
-    "W1":["WANT", "Desire or to lack something."], 
-    "W2":["GENIE", "Aladdin's pottery pal."],
-    "W3":["STAR", "Celestial body or celeb."],
-    "W4":["HOPE", "Faith or optimism."]
+    "W1":["WHITE", "Shade of surrender or snow."], 
+    "W2":["CARVE", "Shape by cutting away."],
+    "W3":["BONE", "Skeletal element."],
+    "W4":["ORNAMENT", "What you might put on a Christmas tree."],
+    "W5":["EBONY", "Dark hardwood, nearly black."]
 };
 
 // Storing the central theme word
-export const Puzzle_ThemeWord = "WISH";
+export const Puzzle_ThemeWord = "IVORY";
 
 // Storing the previous meso for streak purposes
-export const Previous_Meso = "LASER";
+export const Previous_Meso = "WISH";
 
 // Exporting the puzzle author
 export const Puzzle_Author = "Emil Karpinski";
