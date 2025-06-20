32f34cddbc9d160d11873210583dbce218829fee
EmilKarpinski <emil.karpinski.mac@gmail.com>
2025-06-20 09:06:04 -0400

    Today's puzzle

--- src/Puzzle.js
+++ src/Puzzle.js
@@ -1,17 +1,17 @@
 // Storing the puzzle information (words and clues for export)
 export const Puzzle_Word_List = {
-    "W1":["WHITE", "Shade of surrender or snow."], 
-    "W2":["CARVE", "Shape by cutting away."],
-    "W3":["BONE", "Skeletal element."],
-    "W4":["ORNAMENT", "What you might put on a Christmas tree."],
-    "W5":["EBONY", "Dark hardwood, nearly black."]
+    "W1":["RIDE", "What you do on a horse or a bus."], 
+    "W2":["CLOWN", "Found in many circuses or sewers."],
+    "W3":["STAMPEDE", "A panic-driven herd."],
+    "W4":["HORSE", "Faithful steed."],
+    "W5":["COWBOY", "Ranch hand."]
 };
 
 // Storing the central theme word
-export const Puzzle_ThemeWord = "IVORY";
+export const Puzzle_ThemeWord = "RODEO";
 
 // Storing the previous meso for streak purposes
-export const Previous_Meso = "WISH";
+export const Previous_Meso = "IVORY";
 
 // Exporting the puzzle author
 export const Puzzle_Author = "Emil Karpinski";
