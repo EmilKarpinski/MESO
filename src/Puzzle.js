76b1c1b912a7635d340861438b72f7cd2085ec73
EmilKarpinski <emil.karpinski.mac@gmail.com>
2025-06-25 09:16:25 -0400

    Puzzle 2025-06-25

--- src/Puzzle.js
+++ src/Puzzle.js
@@ -1,16 +1,20 @@
 // Storing the puzzle information (words and clues for export)
 export const Puzzle_Word_List = {
-    "W1":["FOUL", "Bad air or bad play."], 
-    "W2":["POLE", "Extreme opposites of Earth."],
-    "W3":["PIRATE", "Sails the physical or electronic high-seas."],
-    "W4":["SIGNAL", "Transmission of intent."]
+    "W1":["MAZE", "Easy to get lost in this."], 
+    "W2":["POSEIDON", "Father of waves."],
+    "W3":["LABYRINTH", "Even more than regular maze."],
+    "W4":["BODY", "The bulk of your physical form, and your essay's."],
+    "W5":["MONSTER", "Evil creature (maybe mythical)."],
+    "W6":["HALF", "50%."],
+    "W7":["BULL", "Headstrong creature with horns."],
+    "W8":["GREEK", "It's all this to someone who doesn't understand."]
 };
 
 // Storing the central theme word
-export const Puzzle_ThemeWord = "FLAG";
+export const Puzzle_ThemeWord = "MINOTAUR";
 
 // Storing the previous meso for streak purposes
-export const Previous_Meso = "BOMB";
+export const Previous_Meso = "FLAG";
 
 // Exporting the puzzle author
 export const Puzzle_Author = "Emil Karpinski";
--- src/PuzzleRepo/UsedPuzzles/Puzzle_MINOTAUR.js
+++ src/PuzzleRepo/UsedPuzzles/Puzzle_MINOTAUR.js
@@ -4,7 +4,7 @@ export const Puzzle_Word_List = {
     "W2":["POSEIDON", "Father of waves."],
     "W3":["LABYRINTH", "Even more than regular maze."],
     "W4":["BODY", "The bulk of your physical form, and your essay's."],
-    "W5":["MONSTER", "Evil creature antagonist."],
+    "W5":["MONSTER", "Evil creature (maybe mythical)."],
     "W6":["HALF", "50%."],
     "W7":["BULL", "Headstrong creature with horns."],
     "W8":["GREEK", "It's all this to someone who doesn't understand."]
@@ -14,7 +14,7 @@ export const Puzzle_Word_List = {
 export const Puzzle_ThemeWord = "MINOTAUR";
 
 // Storing the previous meso for streak purposes
-export const Previous_Meso = "";
+export const Previous_Meso = "FLAG";
 
 // Exporting the puzzle author
 export const Puzzle_Author = "Emil Karpinski";
