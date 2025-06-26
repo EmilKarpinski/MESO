63986bce3de671e1a06982414cad580cc45a01b7
EmilKarpinski <emil.karpinski.mac@gmail.com>
2025-06-26 09:27:15 -0400

    2025-06-26 Puzzle

--- src/Puzzle.js
+++ src/Puzzle.js
@@ -1,20 +1,17 @@
 // Storing the puzzle information (words and clues for export)
 export const Puzzle_Word_List = {
-    "W1":["MAZE", "Easy to get lost in this."], 
-    "W2":["POSEIDON", "Father of waves."],
-    "W3":["LABYRINTH", "Even more than regular maze."],
-    "W4":["BODY", "The bulk of your physical form, and your essay's."],
-    "W5":["MONSTER", "Evil creature (maybe mythical)."],
-    "W6":["HALF", "50%."],
-    "W7":["BULL", "Headstrong creature with horns."],
-    "W8":["GREEK", "It's all this to someone who doesn't understand."]
+    "W1":["HEAVEN", "Paradise above."], 
+    "W2":["HORN", "Animal's pointy projection."],
+    "W3":["WINGS", "Airborne appendages."],
+    "W4":["DEVIL", "Fiend with a pitchfork."],
+    "W5":["HALO", "Divine doughnut."]
 };
 
 // Storing the central theme word
-export const Puzzle_ThemeWord = "MINOTAUR";
+export const Puzzle_ThemeWord = "ANGEL";
 
 // Storing the previous meso for streak purposes
-export const Previous_Meso = "FLAG";
+export const Previous_Meso = "MINOTAUR";
 
 // Exporting the puzzle author
 export const Puzzle_Author = "Emil Karpinski";
