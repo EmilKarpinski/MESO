742a9740e6572a279339eb9736bdbd2e8a690705
EmilKarpinski <emil.karpinski.mac@gmail.com>
2025-06-27 09:49:32 -0400

    2025-06-27

--- src/Puzzle.js
+++ src/Puzzle.js
@@ -1,17 +1,17 @@
 // Storing the puzzle information (words and clues for export)
 export const Puzzle_Word_List = {
-    "W1":["HEAVEN", "Paradise above."], 
-    "W2":["HORN", "Animal's pointy projection."],
-    "W3":["WINGS", "Airborne appendages."],
-    "W4":["DEVIL", "Fiend with a pitchfork."],
-    "W5":["HALO", "Divine doughnut."]
+    "W1":["SPINE", "Keeps you upright."], 
+    "W2":["SYNAPSE", "Tiny gap where neurons meet."],
+    "W3":["BRAIN", "Control center of the body."],
+    "W4":["BRAVERY", "Award winning courage."],
+    "W5":["CELLS", "Organisms and prisons are both made of these."]
 };
 
 // Storing the central theme word
-export const Puzzle_ThemeWord = "ANGEL";
+export const Puzzle_ThemeWord = "NERVE";
 
 // Storing the previous meso for streak purposes
-export const Previous_Meso = "MINOTAUR";
+export const Previous_Meso = "ANGEL";
 
 // Exporting the puzzle author
 export const Puzzle_Author = "Emil Karpinski";
