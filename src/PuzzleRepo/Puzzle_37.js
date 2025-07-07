c07eb61c482f503e59744fcd8517e38f4b45cc12
EmilKarpinski <emil.karpinski.mac@gmail.com>
2025-07-07 10:42:15 -0400

    Some puzzle edits.

--- src/PuzzleRepo/Puzzle_37.js
+++ src/PuzzleRepo/Puzzle_37.js
@@ -1,10 +1,10 @@
 // Storing the puzzle information (words and clues for export)
 export const Puzzle_Word_List = {
-    "W1":["STRATEGY", "Grander than tactics"], 
-    "W2":["PAWN", "A minor, expendable agent"],
-    "W3":["", ""],
-    "W4":["FROGGER", ""],
-    "W5":["SNAKESEYES", "Two ones (two words)"]
+    "W1":["STRATEGY", "Grander than tactics."], 
+    "W2":["PAWN", "A minor, expendable agent."],
+    "W3":["MONOPOLY", "Real-estate deals and arguments on a square board."],
+    "W4":["FROGGER", "Where the virtual frogs crossed the road."],
+    "W5":["SNAKEEYES", "Two ones (two words)"]
 };
 
 // Storing the central theme word
---
+++ src/PuzzleRepo/Puzzle_73.js
@@ -0,0 +1,19 @@
+// Storing the puzzle information (words and clues for export)
+export const Puzzle_Word_List = {
+    "W1":["CALCULATE", ""], 
+    "W2":["OPPOSE", ""],
+    "W3":["MEASURE", ""],
+    "W4":["KITCHEN", ""],
+    "W5":["TALLY", ""],
+    "W6":["RESPOND", ""],
+    "W7":["PARRY", ""]
+};
+
+// Storing the central theme word
+export const Puzzle_ThemeWord = "COUNTER";
+
+// Storing the previous meso for streak purposes
+export const Previous_Meso = "";
+
+// Exporting the puzzle author
+export const Puzzle_Author = "Zachery Dickson";
