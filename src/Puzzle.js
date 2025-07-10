99fca1d2683c3bfe4b575d55890b201f11c02b20
EmilKarpinski <emil.karpinski.mac@gmail.com>
2025-07-10 10:37:22 -0400

    Late puzzle

--- src/Puzzle.js
+++ src/Puzzle.js
@@ -1,18 +1,19 @@
 // Storing the puzzle information (words and clues for export)
 export const Puzzle_Word_List = {
-    "W1":["ARCTIC", "Opposite of Antarctic."], 
-    "W2":["SHRUB", "Small woody plant (categorically)."],
-    "W3":["CANADA", "Where you might find Mounties."],
-    "W4":["DESERT", "Dry sandy cactus home."],
-    "W5":["CARIBOU", "North American reindeer."],
-    "W6":["GLACIER", "A really, really big ice cube."]
+    "W1":["CALCULATE", "To work out a solution (usually with math)."], 
+    "W2":["OPPOSE", "To stand against or "],
+    "W3":["MEASURE", "What you might use a ruler for."],
+    "W4":["KITCHEN", "The hearth of the home."],
+    "W5":["TALLY", "A running count of stuff."],
+    "W6":["RESPOND", "Reply to a letter or an invitation."],
+    "W7":["PARRY", "Quick deflection of an attack."]
 };
 
 // Storing the central theme word
-export const Puzzle_ThemeWord = "TUNDRA";
+export const Puzzle_ThemeWord = "COUNTER";
 
 // Storing the previous meso for streak purposes
-export const Previous_Meso = "GAMES";
+export const Previous_Meso = "TUNDRA";
 
 // Exporting the puzzle author
-export const Puzzle_Author = "Sophie Reilly";
+export const Puzzle_Author = "Zachery Dickson";
