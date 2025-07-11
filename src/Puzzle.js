55e8e4483f5eeafef95e4452a021a125ad3940bd
EmilKarpinski <emil.karpinski.mac@gmail.com>
2025-07-11 10:00:18 -0400

    2025-07-11

--- src/Puzzle.js
+++ src/Puzzle.js
@@ -1,19 +1,18 @@
 // Storing the puzzle information (words and clues for export)
 export const Puzzle_Word_List = {
-    "W1":["CALCULATE", "To work out a solution (usually with math)."], 
-    "W2":["OPPOSE", "To stand against. ie. to ______"],
-    "W3":["MEASURE", "What you might use a ruler for."],
-    "W4":["KITCHEN", "The hearth of the home."],
-    "W5":["TALLY", "A running count of stuff."],
-    "W6":["RESPOND", "Reply to a letter or an invitation."],
-    "W7":["PARRY", "Quick deflection of an attack."]
+    "W1":["HUGO", "Worldcon award for best science fiction."], 
+    "W2":["OSCAR", "A little golden man who loves the movies."],
+    "W3":["NEBULA", "A space cloud."],
+    "W4":["NOBEL", "Prestigious award in physics, chemistry, literature, or medicine."],
+    "W5":["PULITZER", "Outstanding achievment in American journalism and literary arts."],
+    "W6":["AWARDS", "What might be handed out after a competition or for as prizes."]
 };
 
 // Storing the central theme word
-export const Puzzle_ThemeWord = "COUNTER";
+export const Puzzle_ThemeWord = "HONORS";
 
 // Storing the previous meso for streak purposes
-export const Previous_Meso = "TUNDRA";
+export const Previous_Meso = "COUNTER";
 
 // Exporting the puzzle author
-export const Puzzle_Author = "Zachery Dickson";
+export const Puzzle_Author = "Beth Green"
