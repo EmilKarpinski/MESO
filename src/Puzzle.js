d2e6ebc7c228de431d52f051fa5904248b39e887
EmilKarpinski <emil.karpinski.mac@gmail.com>
2025-07-14 09:27:14 -0400

    2025-07-14

--- src/Puzzle.js
+++ src/Puzzle.js
@@ -1,18 +1,15 @@
 // Storing the puzzle information (words and clues for export)
 export const Puzzle_Word_List = {
-    "W1":["HUGO", "Worldcon award for best science fiction."], 
-    "W2":["OSCAR", "A little golden man who loves the movies."],
-    "W3":["NEBULA", "A space cloud."],
-    "W4":["NOBEL", "Prestigious award in physics, chemistry, literature, or medicine."],
-    "W5":["PULITZER", "Outstanding achievment in American journalism and literary arts."],
-    "W6":["AWARDS", "What might be handed out after a competition or for as prizes."]
+    "W1":["SPOT", "Polka dots or found on dalmatians."], 
+    "W2":["UV", "Harmful outdoor rays."],
+    "W3":["DAWN", "Breaks the night."]
 };
 
 // Storing the central theme word
-export const Puzzle_ThemeWord = "HONORS";
+export const Puzzle_ThemeWord = "SUN";
 
 // Storing the previous meso for streak purposes
-export const Previous_Meso = "COUNTER";
+export const Previous_Meso = "HONORS";
 
 // Exporting the puzzle author
-export const Puzzle_Author = "Beth Green";
+export const Puzzle_Author = "Emil Karpinski";
--- src/PuzzleRepo/UsedPuzzles/Puzzle_SUN.js
+++ src/PuzzleRepo/UsedPuzzles/Puzzle_SUN.js
@@ -9,7 +9,7 @@ export const Puzzle_Word_List = {
 export const Puzzle_ThemeWord = "SUN";
 
 // Storing the previous meso for streak purposes
-export const Previous_Meso = "";
+export const Previous_Meso = "HONORS";
 
 // Exporting the puzzle author
 export const Puzzle_Author = "Emil Karpinski";
