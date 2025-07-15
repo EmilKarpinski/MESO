d6926202ef61955ed7fd053f8cb31c5514e3ec20
EmilKarpinski <emil.karpinski.mac@gmail.com>
2025-07-15 09:03:01 -0400

    2025-07-15

--- src/Puzzle.js
+++ src/Puzzle.js
@@ -1,15 +1,18 @@
+// This PUZZLE IS CURRENTLY NONFUNCTIONAL
+
 // Storing the puzzle information (words and clues for export)
 export const Puzzle_Word_List = {
-    "W1":["SPOT", "Polka dots or found on dalmatians."], 
-    "W2":["UV", "Harmful outdoor rays."],
-    "W3":["DAWN", "Breaks the night."]
+    "W1":["THRESHOLD", "On the brink of something. i.e. the _________"], 
+    "W2":["LOCK", "Needs a key or a code."],
+    "W3":["KNOB", "Twist to open or set volume."],
+    "W4":["TRAP", "Hidden snare or pitfall."]
 };
 
 // Storing the central theme word
-export const Puzzle_ThemeWord = "SUN";
+export const Puzzle_ThemeWord = "DOOR";
 
 // Storing the previous meso for streak purposes
-export const Previous_Meso = "HONORS";
+export const Previous_Meso = "SUN";
 
 // Exporting the puzzle author
 export const Puzzle_Author = "Emil Karpinski";
