d06f3f6fc186b51ea4ffcb43efb48892b734359c
EmilKarpinski <emil.karpinski.mac@gmail.com>
2025-07-16 09:32:10 -0400

    2025-07-16

--- src/Puzzle.js
+++ src/Puzzle.js
@@ -1,18 +1,16 @@
-// This PUZZLE IS CURRENTLY NONFUNCTIONAL
-
 // Storing the puzzle information (words and clues for export)
 export const Puzzle_Word_List = {
-    "W1":["THRESHOLD", "On the brink of something. i.e. the _________"], 
-    "W2":["LOCK", "Needs a key or a code."],
-    "W3":["KNOB", "Twist to open or set volume."],
-    "W4":["TRAP", "Hidden snare or pitfall."]
+    "W1":["KHAN", "Central asian ruler title."], 
+    "W2":["SOVEREIGN", "Royal or independant."],
+    "W3":["CROWN", "The top of the head or something placed atop the top of the head."],
+    "W4":["ENGLAND", "Non-Italian home to an old pre-Italian wall."]
 };
 
 // Storing the central theme word
-export const Puzzle_ThemeWord = "DOOR";
+export const Puzzle_ThemeWord = "KING";
 
 // Storing the previous meso for streak purposes
-export const Previous_Meso = "SUN";
+export const Previous_Meso = "DOOR";
 
 // Exporting the puzzle author
 export const Puzzle_Author = "Emil Karpinski";
--- src/PuzzleRepo/UsedPuzzles/Puzzle_KING.js
+++ src/PuzzleRepo/UsedPuzzles/Puzzle_KING.js
@@ -1,16 +1,16 @@
 // Storing the puzzle information (words and clues for export)
 export const Puzzle_Word_List = {
-    "W1":["KHAN", ""], 
-    "W2":["SOVEREIGN", ""],
-    "W3":["CROWN", ""],
-    "W4":["ENGLAND", ""]
+    "W1":["KHAN", "Central asian ruler title."], 
+    "W2":["SOVEREIGN", "Royal or independant."],
+    "W3":["CROWN", "The top of the head or something placed atop the top of the head."],
+    "W4":["ENGLAND", "Non-Italian home to an old pre-Italian wall."]
 };
 
 // Storing the central theme word
 export const Puzzle_ThemeWord = "KING";
 
 // Storing the previous meso for streak purposes
-export const Previous_Meso = "";
+export const Previous_Meso = "DOOR";
 
 // Exporting the puzzle author
 export const Puzzle_Author = "Emil Karpinski";
