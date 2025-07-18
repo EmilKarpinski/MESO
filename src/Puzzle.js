08dee4a26a76604b3396824e8de4c1ead6865420
EmilKarpinski <emil.karpinski.mac@gmail.com>
2025-07-18 09:47:53 -0400

    2025-07-17

--- src/Puzzle.js
+++ src/Puzzle.js
@@ -1,18 +1,19 @@
 // Storing the puzzle information (words and clues for export)
 export const Puzzle_Word_List = {
-    "W1":["WICK", "Lit rope."], 
-    "W2":["WAX", "Used to seal very fancy letters."],
-    "W3":["ROMANTIC", "Loving stories or suppers."],
-    "W4":["DINNER", "Meal after lunch."],
-    "W5":["FLAME", "Burning tongue of fire."],
-    "W6":["MELT", "Turn from solid to liquid."]
+    "W1":["HEATWAVE", "Sweltering streak."], 
+    "W2":["SEASON", "Quarter of a year."],
+    "W3":["HAIL", "Glacial pelleting pellets."],
+    "W4":["LIGHTNING", "Seen before it's heard."],
+    "W5":["DROUGHT", "Parched period."],
+    "W6":["SUNSHINE", "Ray of light or optimism."],
+    "W7":["RADAR", "Terrestrial sonar."]
 };
 
 // Storing the central theme word
-export const Puzzle_ThemeWord = "CANDLE";
+export const Puzzle_ThemeWord = "WEATHER";
 
 // Storing the previous meso for streak purposes
-export const Previous_Meso = "KING";
+export const Previous_Meso = "CANDLE";
 
 // Exporting the puzzle author
-export const Puzzle_Author = "Emil Karpinski";
+export const Puzzle_Author = "Sophie Reilly"
