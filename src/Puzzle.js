77382790b997c49c62b00dcc72caf952b959aa29
EmilKarpinski <emil.karpinski.mac@gmail.com>
2025-07-22 09:39:10 -0400

    2025-07-22

--- src/Puzzle.js
+++ src/Puzzle.js
@@ -1,16 +1,17 @@
 // Storing the puzzle information (words and clues for export)
 export const Puzzle_Word_List = {
-    "W1":["SNOW", "White winter blanket."], 
-    "W2":["LUGE", "Icy Olympic thrill ride."],
-    "W3":["REINDEER", "Christmasy caribou."],
-    "W4":["DOGS", "Furry friends."]
+    "W1":["PHONE", "Movie theater interrupter to be silenced."], 
+    "W2":["SHOULDER", "Cry on it or carry with it."],
+    "W3":["TRAVEL", "What a door-to-door salesman might do."],
+    "W4":["STRAP", "Cool schoolkids might only use one of two."],
+    "W5":["WALLET", "Pocket-sized bank."]
 };
 
 // Storing the central theme word
-export const Puzzle_ThemeWord = "SLED";
+export const Puzzle_ThemeWord = "PURSE";
 
 // Storing the previous meso for streak purposes
-export const Previous_Meso = "WEATHER";
+export const Previous_Meso = "SLED";
 
 // Exporting the puzzle author
 export const Puzzle_Author = "Emil Karpinski";
