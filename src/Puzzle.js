a7b86b2b810f4f04ef418d8116fbbd0b04fbb6df
EmilKarpinski <emil.karpinski.mac@gmail.com>
2025-07-25 09:37:04 -0400

    2025-07-25

--- src/Puzzle.js
+++ src/Puzzle.js
@@ -1,16 +1,16 @@
 // Storing the puzzle information (words and clues for export)
 export const Puzzle_Word_List = {
-    "W1":["LEFT", "Not right."], 
-    "W2":["SOCKS", "Often lost in the laundry."],
-    "W3":["TOES", "Needed to count past 10."],
-    "W4":["WART", "Hallmark of witches and toads."]
+    "W1":["SKIRT", "Waist-bound garment for twirling "], 
+    "W2":["COSTUME", "Outfit for Halloween or the theater."],
+    "W3":["BALLET", "Dance on tiptoe."],
+    "W4":["UNITARD", "One-piece dancewear."]
 };
 
 // Storing the central theme word
-export const Puzzle_ThemeWord = "FOOT";
+export const Puzzle_ThemeWord = "TUTU";
 
 // Storing the previous meso for streak purposes
-export const Previous_Meso = "BASS";
+export const Previous_Meso = "FOOT";
 
 // Exporting the puzzle author
 export const Puzzle_Author = "Emil Karpinski";
