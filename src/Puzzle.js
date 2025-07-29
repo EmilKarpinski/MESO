a11bb4bbe36ff01defe6107b257f4e9002d46ee0
EmilKarpinski <emil.karpinski.mac@gmail.com>
2025-07-29 09:09:45 -0400

    2025-07-29

--- src/Puzzle.js
+++ src/Puzzle.js
@@ -1,16 +1,15 @@
 // Storing the puzzle information (words and clues for export)
 export const Puzzle_Word_List = {
-    "W1":["FLAME", "Torch bearer's burden."], 
-    "W2":["IGNITE", "Light up (a match or a movement)"],
-    "W3":["SPARK", "Electric or from striking rocks."],
-    "W4":["HEAT", "You might run in one during a race, or from them after a crime."]
+    "W1":["TEAPOT", "Shape of Belle's tea-time companion."], 
+    "W2":["LIVESTOCK", "Farmer's four-legged assets."],
+    "W3":["HOG", "Greedy type or swine."]
 };
 
 // Storing the central theme word
-export const Puzzle_ThemeWord = "FIRE";
+export const Puzzle_ThemeWord = "PIG";
 
 // Storing the previous meso for streak purposes
-export const Previous_Meso = "TUTU";
+export const Previous_Meso = "FIRE";
 
 // Exporting the puzzle author
 export const Puzzle_Author = "Emil Karpinski";
