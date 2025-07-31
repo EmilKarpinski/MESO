89dfbce94e1f58bcf2956b49420513028394f053
EmilKarpinski <emil.karpinski.mac@gmail.com>
2025-07-31 09:59:31 -0400

    2025-07-31

--- src/Puzzle.js
+++ src/Puzzle.js
@@ -1,17 +1,20 @@
 // Storing the puzzle information (words and clues for export)
 export const Puzzle_Word_List = {
-    "W1":["PAJAMA", "Nighttime attire."], 
-    "W2":["SLACKS", "Business casual bottoms."],
-    "W3":["JEANS", "Half a canadian tuxedo."],
-    "W4":["SHORTS", "Half-off bottoms!"],
-    "W5":["DRESS", "Flowy or fitted frock."]
+    "W1":["PERU", "Lima land full of llamas."], 
+    "W2":["ROOT", "Math function or plant part."],
+    "W3":["PLANT", "Grows or manufactures."],
+    "W4":["VODKA", "Liquid courage, Slavic edition?"],
+    "W5":["STARCH", "Corn power-d thickener."],
+    "W6":["GNOCCHI", "Little carb-full pillows."],
+    "W7":["IRELAND", "Green isle with a rich history."],
+    "W8":["CHIPS", "Poker or edible varieties."]
 };
 
 // Storing the central theme word
-export const Puzzle_ThemeWord = "PANTS";
+export const Puzzle_ThemeWord = "POTATOES";
 
 // Storing the previous meso for streak purposes
-export const Previous_Meso = "PIG";
+export const Previous_Meso = "PANTS";
 
 // Exporting the puzzle author
-export const Puzzle_Author = "Emil Karpinski";
+export const Puzzle_Author = "Sophie Reilly"
