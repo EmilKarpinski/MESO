2e7dfb6046bac994ab328f15f48f450d39056ef5
EmilKarpinski <emil.karpinski.mac@gmail.com>
2025-08-01 09:54:05 -0400

    2025-08-01

--- src/Puzzle.js
+++ src/Puzzle.js
@@ -1,20 +1,20 @@
 // Storing the puzzle information (words and clues for export)
 export const Puzzle_Word_List = {
-    "W1":["PERU", "Lima land full of llamas."], 
-    "W2":["ROOT", "Math function or plant part."],
-    "W3":["PLANT", "Grows or manufactures."],
-    "W4":["VODKA", "Liquid courage, Slavic edition?"],
-    "W5":["STARCH", "Corn power-d thickener."],
-    "W6":["GNOCCHI", "Little carb-full pillows."],
-    "W7":["IRELAND", "Green isle with a rich history."],
-    "W8":["CHIPS", "Poker or edible varieties."]
+    "W1":["FRINGE", "Alternative arts or English bangs."], 
+    "W2":["CONCERT", "A ticketed singing show."],
+    "W3":["MUSIC", "What a band produces, ideally."],
+    "W4":["OUTDOORS", "Hiker's habitat."],
+    "W5":["FAIR", "Place which might have rides and ribbons."],
+    "W6":["HARVEST", "Aka to reap what you've sown."],
+    "W7":["STAGE", "Where the actors play."],
+    "W8":["ANNUAL", "Once a year."]
 };
 
 // Storing the central theme word
-export const Puzzle_ThemeWord = "POTATOES";
+export const Puzzle_ThemeWord = "FESTIVAL";
 
 // Storing the previous meso for streak purposes
-export const Previous_Meso = "PANTS";
+export const Previous_Meso = "POTATOES";
 
 // Exporting the puzzle author
 export const Puzzle_Author = "Sophie Reilly"
