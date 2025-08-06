3b8ffd7d8fd06e7057281f697f510684c75ade73
EmilKarpinski <emil.karpinski.mac@gmail.com>
2025-08-06 09:22:02 -0400

    Two more puzzles.

---
+++ src/PuzzleRepo/Puzzle_BERRIES.js
@@ -0,0 +1,19 @@
+// Storing the puzzle information (words and clues for export)
+export const Puzzle_Word_List = {
+    "W1":["BLACK", "Absence of light."], 
+    "W2":["BLUE", "Feeling down."],
+    "W3":["RASP", "Harsh grating noise."],
+    "W4":["STRAW", "Plastic or paper drinkware."],
+    "W5":["WILD","Where the ____ things are."],
+    "W6":["ELDER","Aged source of wisdom."],
+    "W7":["GOOSE","Known for honking and V formations."]
+};
+
+// Storing the central theme word
+export const Puzzle_ThemeWord = "BERRIES";
+
+// Storing the previous meso for streak purposes
+export const Previous_Meso = "";
+
+// Exporting the puzzle author
+export const Puzzle_Author = "Sophie Reilly";
---
+++ src/PuzzleRepo/Puzzle_CHAIR.js
@@ -0,0 +1,17 @@
+// Storing the puzzle information (words and clues for export)
+export const Puzzle_Word_List = {
+    "W1":["OFFICE", "Where you go to work."], 
+    "W2":["COUCH", "Larger loveseat."],
+    "W3":["ARM", "Not your leg."],
+    "W4":["ROCKING", "_______ around the Christmas tree."],
+    "W5":["BOARD", "Surf_____ or _____walk"]
+};
+
+// Storing the central theme word
+export const Puzzle_ThemeWord = "CHAIR";
+
+// Storing the previous meso for streak purposes
+export const Previous_Meso = "";
+
+// Exporting the puzzle author
+export const Puzzle_Author = "Sophie Reilly";
