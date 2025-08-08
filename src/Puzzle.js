c04177c38c33035b6c6d87cd81681e4d9a97516d
EmilKarpinski <emil.karpinski.mac@gmail.com>
2025-08-08 09:28:33 -0400

    2025-08-08

--- src/Puzzle.js
+++ src/Puzzle.js
@@ -1,17 +1,19 @@
 // Storing the puzzle information (words and clues for export)
 export const Puzzle_Word_List = {
-    "W1":["OFFICE", "Where you go to work."], 
-    "W2":["COUCH", "Larger loveseat."],
-    "W3":["ARM", "Not your leg."],
-    "W4":["ROCKING", "_______ around the Christmas tree."],
-    "W5":["BOARD", "Surf_____ or _____walk"]
+    "W1":["BLACK", "Absence of light."], 
+    "W2":["BLUE", "Feeling down."],
+    "W3":["RASP", "Harsh grating noise."],
+    "W4":["STRAW", "Plastic or paper drinkware."],
+    "W5":["WILD","Where the ____ things are."],
+    "W6":["ELDER","Aged source of wisdom."],
+    "W7":["GOOSE","Known for honking and V formations. (Singular)"]
 };
 
 // Storing the central theme word
-export const Puzzle_ThemeWord = "CHAIR";
+export const Puzzle_ThemeWord = "BERRIES";
 
 // Storing the previous meso for streak purposes
-export const Previous_Meso = "CARDS";
+export const Previous_Meso = "CHAIR";
 
 // Exporting the puzzle author
 export const Puzzle_Author = "Sophie Reilly";
--- src/PuzzleRepo/UsedPuzzles/Puzzle_BERRIES.js
+++ src/PuzzleRepo/UsedPuzzles/Puzzle_BERRIES.js
@@ -6,14 +6,14 @@ export const Puzzle_Word_List = {
     "W4":["STRAW", "Plastic or paper drinkware."],
     "W5":["WILD","Where the ____ things are."],
     "W6":["ELDER","Aged source of wisdom."],
-    "W7":["GOOSE","Known for honking and V formations."]
+    "W7":["GOOSE","Known for honking and V formations. (Singular)"]
 };
 
 // Storing the central theme word
 export const Puzzle_ThemeWord = "BERRIES";
 
 // Storing the previous meso for streak purposes
-export const Previous_Meso = "";
+export const Previous_Meso = "CHAIR";
 
 // Exporting the puzzle author
 export const Puzzle_Author = "Sophie Reilly";
