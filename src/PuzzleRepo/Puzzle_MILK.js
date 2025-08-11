73aab2677bc84a384193d744a1a40f48ff99fb1f
EmilKarpinski <emil.karpinski.mac@gmail.com>
2025-08-11 07:47:53 -0400

    Milk puzzle

---
+++ src/PuzzleRepo/Puzzle_MILK.js
@@ -0,0 +1,16 @@
+// Storing the puzzle information (words and clues for export)
+export const Puzzle_Word_List = {
+    "W1":["CREAM", "Rises to the top."], 
+    "W2":["SKIM", "Remove from the surface to clean or enrich yourself."],
+    "W3":["WHOLE", "Unbroken or complete."],
+    "W4":["DRINK", "Take a swig."]
+};
+
+// Storing the central theme word
+export const Puzzle_ThemeWord = "MILK";
+
+// Storing the previous meso for streak purposes
+export const Previous_Meso = "BERRIES";
+
+// Exporting the puzzle author
+export const Puzzle_Author = "Emil Karpinski";
