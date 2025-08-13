86130358be46fbde71e5135732ee5b4c06055866
Emil Karpinski <102920028+EmilKarpinski@users.noreply.github.com>
2025-08-13 10:12:41 -0400

    Update Puzzle.js

--- src/Puzzle.js
+++ src/Puzzle.js
@@ -1,17 +1,18 @@
 // Storing the puzzle information (words and clues for export)
 export const Puzzle_Word_List = {
-    "W1":["MELODY", "Catchy musical line."], 
-    "W2":["CHORUS", "Repeated section in a song."],
-    "W3":["SINGER", "Preformer witth pipes."],
-    "W4":["LYRICS", "Musical poetry."],
-    "W5":["ORCHESTRA", "Conductor's crew."]
+    "W1":["BEER", "Cold one?"], 
+    "W2":["ROCKET", "Firework or spacecraft."],
+    "W3":["WATER", "Ocean content."],
+    "W4":["PLASTIC", "Makes and means credit cards."],
+    "W5":["GLASS", "Toasting vessel."],
+    "W6":["WINE", "Vintage drink."]
 };
 
 // Storing the central theme word
-export const Puzzle_ThemeWord = "MUSIC";
+export const Puzzle_ThemeWord = "BOTTLE";
 
 // Storing the previous meso for streak purposes
-export const Previous_Meso = "MILK";
+export const Previous_Meso = "MUSIC";
 
 // Exporting the puzzle author
 export const Puzzle_Author = "Emil Karpinski";
