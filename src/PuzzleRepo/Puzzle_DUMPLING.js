0f37dbcc31f77478a3afef277d39038633b6f834
EmilKarpinski <emil.karpinski.mac@gmail.com>
2025-08-12 21:33:40 -0400

    Dumpling partial puzzle

---
+++ src/PuzzleRepo/Puzzle_DUMPLING.js
@@ -0,0 +1,20 @@
+// Storing the puzzle information (words and clues for export)
+export const Puzzle_Word_List = {
+    "W1":["FRIED", ""], 
+    "W2":["DIMSUM", ""],
+    "W3":["SAMOSA", ""],
+    "W4":["WRAPPER", ""],
+    "W5":["FILLING", ""],
+    "W6":["PIEROGI", ""],
+    "W7":["EMPANADA", ""],
+    "W8":["GNOCCHI", ""]
+};
+
+// Storing the central theme word
+export const Puzzle_ThemeWord = "DUMPLING";
+
+// Storing the previous meso for streak purposes
+export const Previous_Meso = "";
+
+// Exporting the puzzle author
+export const Puzzle_Author = "Emil Karpinski";
