3129e7c8c7d232dfb61862e1fd254eae26e7621e
EmilKarpinski <emil.karpinski.mac@gmail.com>
2025-08-13 21:29:11 -0400

    small update to the dumpling puzzle

--- src/PuzzleRepo/Puzzle_DUMPLING.js
+++ src/PuzzleRepo/Puzzle_DUMPLING.js
@@ -14,7 +14,7 @@ export const Puzzle_Word_List = {
 export const Puzzle_ThemeWord = "DUMPLING";
 
 // Storing the previous meso for streak purposes
-export const Previous_Meso = "";
+export const Previous_Meso = "BOTTLE";
 
 // Exporting the puzzle author
 export const Puzzle_Author = "Emil Karpinski";
