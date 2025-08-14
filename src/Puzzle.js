b6d120461c46dde693905791b80391ece818ce5d
Emil Karpinski <102920028+EmilKarpinski@users.noreply.github.com>
2025-08-14 09:04:53 -0400

    Dumpling puzzle

--- src/Puzzle.js
+++ src/Puzzle.js
@@ -1,18 +1,20 @@
 // Storing the puzzle information (words and clues for export)
 export const Puzzle_Word_List = {
-    "W1":["BEER", "Cold one?"], 
-    "W2":["ROCKET", "Firework or spacecraft."],
-    "W3":["WATER", "Ocean content."],
-    "W4":["PLASTIC", "Makes and means credit cards."],
-    "W5":["GLASS", "Toasting vessel."],
-    "W6":["WINE", "Vintage drink."]
+    "W1":["DIMSUM", "Assorted Chinese bites, often shared (2 words)."], 
+    "W2":["FUFU", "African starchy soup add-in."],
+    "W3":["SAMOSA", "Triangle-shaped Indian snack."],
+    "W4":["WRAPPER", "Edible casing."],
+    "W5":["FILLING", "e.g. Spiced meat or veggies."],
+    "W6":["PIEROGI", "Half-moon shaped Eastern European dough pockets."],
+    "W7":["EMPANADA", "Stuffed pastry found across Latin America."],
+    "W8":["GNOCCHI", "Pillowy pasta bites often served with sauce."]
 };
 
 // Storing the central theme word
-export const Puzzle_ThemeWord = "BOTTLE";
+export const Puzzle_ThemeWord = "DUMPLING";
 
 // Storing the previous meso for streak purposes
-export const Previous_Meso = "MUSIC";
+export const Previous_Meso = "BOTTLE";
 
 // Exporting the puzzle author
 export const Puzzle_Author = "Emil Karpinski";
