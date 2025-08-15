b02e96c76eb21e5d413b419adc7361bb80c7b590
EmilKarpinski <emil.karpinski.mac@gmail.com>
2025-08-15 09:36:31 -0400

    2025-08-15

--- src/Puzzle.js
+++ src/Puzzle.js
@@ -1,20 +1,19 @@
 // Storing the puzzle information (words and clues for export)
 export const Puzzle_Word_List = {
-    "W1":["DIMSUM", "Assorted Chinese bites, often shared (2 words)."], 
-    "W2":["FUFU", "African starchy soup add-in."],
-    "W3":["SAMOSA", "Triangle-shaped Indian snack."],
-    "W4":["WRAPPER", "Edible casing."],
-    "W5":["FILLING", "e.g. Spiced meat or veggies."],
-    "W6":["PIEROGI", "Half-moon shaped Eastern European dough pockets."],
-    "W7":["EMPANADA", "Stuffed pastry found across Latin America."],
-    "W8":["GNOCCHI", "Pillowy pasta bites often served with sauce."]
+    "W1":["DRUNK", "Sloshed or smashed."], 
+    "W2":["BAR", "Pub synonym."],
+    "W3":["LYRICS", "Singable script."],
+    "W4":["JAPAN", "Home of cherry blossoms and bullet trains."],
+    "W5":["SONG", "Something you might sing."],
+    "W6":["JUKEBOX", "Coin operated DJ."],
+    "W7":["CONCERT", "Show of harmony?"]
 };
 
 // Storing the central theme word
-export const Puzzle_ThemeWord = "DUMPLING";
+export const Puzzle_ThemeWord = "KARAOKE";
 
 // Storing the previous meso for streak purposes
-export const Previous_Meso = "BOTTLE";
+export const Previous_Meso = "DUMPLING";
 
 // Exporting the puzzle author
 export const Puzzle_Author = "Emil Karpinski";
--- src/PuzzleRepo/UsedPuzzles/Puzzle_KARAOKE.js
+++ src/PuzzleRepo/UsedPuzzles/Puzzle_KARAOKE.js
@@ -13,7 +13,7 @@ export const Puzzle_Word_List = {
 export const Puzzle_ThemeWord = "KARAOKE";
 
 // Storing the previous meso for streak purposes
-export const Previous_Meso = "CHOIR";
+export const Previous_Meso = "DUMPLING";
 
 // Exporting the puzzle author
 export const Puzzle_Author = "Emil Karpinski";
