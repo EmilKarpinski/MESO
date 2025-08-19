318d30fc5014fd572e359a9ebf0a5550e1bdc7b1
EmilKarpinski <emil.karpinski.mac@gmail.com>
2025-08-19 09:44:35 -0400

    2025-08-19

--- src/Puzzle.js
+++ src/Puzzle.js
@@ -1,16 +1,16 @@
 // Storing the puzzle information (words and clues for export)
 export const Puzzle_Word_List = {
-    "W1":["CLOVER", "Four-leaf charm."], 
-    "W2":["FORTUNE", "Great wealth or luck."],
-    "W3":["CHANCE", "A random possibility."],
-    "W4":["FLUKE", "Unlikely success."]
+    "W1":["RANDOM", "Unpredictable result."], 
+    "W2":["PIP", "Small see or mark."],
+    "W3":["LUCK", "Can be good, can be bad. Better when it's good."],
+    "W4":["CUBE", "6 faces and 12 edges."]
 };
 
 // Storing the central theme word
-export const Puzzle_ThemeWord = "LUCK";
+export const Puzzle_ThemeWord = "DICE";
 
 // Storing the previous meso for streak purposes
-export const Previous_Meso = "KARAOKE";
+export const Previous_Meso = "LUCK";
 
 // Exporting the puzzle author
 export const Puzzle_Author = "Emil Karpinski";
