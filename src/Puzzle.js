1adb692d4f468fb5534b2a9b42f47ad15a4973b5
EmilKarpinski <emil.karpinski.mac@gmail.com>
2025-08-25 10:10:13 -0400

    2025-08-25

--- src/Puzzle.js
+++ src/Puzzle.js
@@ -1,16 +1,15 @@
 // Storing the puzzle information (words and clues for export)
 export const Puzzle_Word_List = {
-    "W1":["POCKET", "Small place for loose change or keys."], 
-    "W2":["TOURNEY", "A short tournament (literally)."],
-    "W3":["PARLOR", "Old-timey social room."],
-    "W4":["TABLE", "A place for a meeting or to temporarily end a topic during that meeting."]
+    "W1":["ALMOND", "Main part of marzipan."], 
+    "W2":["BUTTER", "Spread that's churned."],
+    "W3":["PISTACHIO", "Flavor of gelator or baklava."]
 };
 
 // Storing the central theme word
-export const Puzzle_ThemeWord = "POOL";
+export const Puzzle_ThemeWord = "NUT";
 
 // Storing the previous meso for streak purposes
-export const Previous_Meso = "RADIO";
+export const Previous_Meso = "POOL";
 
 // Exporting the puzzle author
 export const Puzzle_Author = "Emil Karpinski";
