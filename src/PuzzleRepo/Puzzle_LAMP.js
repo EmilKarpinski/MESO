a583625be917eb1a1d8c07f1ee9264954ba42758
EmilKarpinski <emil.karpinski.mac@gmail.com>
2025-08-25 21:47:56 -0400

    Lamp puzzle for tomorrow

---
+++ src/PuzzleRepo/Puzzle_LAMP.js
@@ -0,0 +1,16 @@
+// Storing the puzzle information (words and clues for export)
+export const Puzzle_Word_List = {
+    "W1":["BULB", "Plant starte or source of light."], 
+    "W2":["SHADE", "Thrown by trees or tweets."],
+    "W3":["MOTH", "Fatally attracted to the light."],
+    "W4":["APPLIANCE", "Plugged in domestic device."]
+};
+
+// Storing the central theme word
+export const Puzzle_ThemeWord = "LAMP";
+
+// Storing the previous meso for streak purposes
+export const Previous_Meso = "NUT";
+
+// Exporting the puzzle author
+export const Puzzle_Author = "Emil Karpinski";
