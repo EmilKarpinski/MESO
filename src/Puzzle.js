3b37ea5b8f5a76e259c2510f70e1e78ed2017edc
EmilKarpinski <emil.karpinski.mac@gmail.com>
2025-08-27 09:36:21 -0400

    2025-08-27

--- src/Puzzle.js
+++ src/Puzzle.js
@@ -1,16 +1,17 @@
 // Storing the puzzle information (words and clues for export)
 export const Puzzle_Word_List = {
-    "W1":["BULB", "Plant starter or source of light."], 
-    "W2":["SHADE", "Thrown by trees or tweets."],
-    "W3":["MOTH", "Fatally attracted to the light."],
-    "W4":["APPLIANCE", "Plugged in domestic device."]
+    "W1":["SNOW", "Flurries."], 
+    "W2":["LIGHTNING", "Frightening flash followed by a bang."],
+    "W3":["CLOUD", "Puffy sky shape."],
+    "W4":["THUNDER", "Boom after the bolt."],
+    "W5":["MONSOON", "Asia's annual drenching period."]
 };
 
 // Storing the central theme word
-export const Puzzle_ThemeWord = "LAMP";
+export const Puzzle_ThemeWord = "STORM";
 
 // Storing the previous meso for streak purposes
-export const Previous_Meso = "NUT";
+export const Previous_Meso = "LAMP";
 
 // Exporting the puzzle author
 export const Puzzle_Author = "Emil Karpinski";
--- src/PuzzleRepo/UsedPuzzles/Puzzle_STORM.js
+++ src/PuzzleRepo/UsedPuzzles/Puzzle_STORM.js
@@ -2,7 +2,7 @@
 export const Puzzle_Word_List = {
     "W1":["SNOW", "Flurries."], 
     "W2":["LIGHTNING", "Frightening flash followed by a bang."],
-    "W3":["CLOUD", "Puffy sky shapes."],
+    "W3":["CLOUD", "Puffy sky shape."],
     "W4":["THUNDER", "Boom after the bolt."],
     "W5":["MONSOON", "Asia's annual drenching period."]
 };
