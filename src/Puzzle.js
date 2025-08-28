09dd36013b1ff009ca563f65ad8e78170077b1ee
EmilKarpinski <emil.karpinski.mac@gmail.com>
2025-08-28 09:48:59 -0400

    2025-08-28

--- src/Puzzle.js
+++ src/Puzzle.js
@@ -1,17 +1,15 @@
 // Storing the puzzle information (words and clues for export)
 export const Puzzle_Word_List = {
-    "W1":["SNOW", "Flurries."], 
-    "W2":["LIGHTNING", "Frightening flash followed by a bang."],
-    "W3":["CLOUD", "Puffy sky shape."],
-    "W4":["THUNDER", "Boom after the bolt."],
-    "W5":["MONSOON", "Asia's annual drenching period."]
+    "W1":["COCONUT", "Tree's bowling ball?"], 
+    "W2":["OLIVE", "Peaceful garnish."],
+    "W3":["VEGETABLE", "Plant-based side."]
 };
 
 // Storing the central theme word
-export const Puzzle_ThemeWord = "STORM";
+export const Puzzle_ThemeWord = "OIL";
 
 // Storing the previous meso for streak purposes
-export const Previous_Meso = "LAMP";
+export const Previous_Meso = "STORM";
 
 // Exporting the puzzle author
 export const Puzzle_Author = "Emil Karpinski";
