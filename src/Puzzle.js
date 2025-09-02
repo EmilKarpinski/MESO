6dae83edc9fd6e1baf28f137c417121233374bb3
EmilKarpinski <emil.karpinski.mac@gmail.com>
2025-09-02 09:28:12 -0400

    2025-09-02

--- src/Puzzle.js
+++ src/Puzzle.js
@@ -1,17 +1,17 @@
 // Storing the puzzle information (words and clues for export)
 export const Puzzle_Word_List = {
-    "W1":["SKILL", "An action or thing you might be particularly good at."], 
-    "W2":["TRADE", "To barter items or time."],
-    "W3":["JOB", "9-to-5 appointment."],
-    "W4":["UNION", "A strong collective."],
-    "W5":["WORK", "Things to do."]
+    "W1":["SPICY", "Hot in taste or substance."], 
+    "W2":["DANCE", "To move rythmically."],
+    "W3":["LATIN", "Old language of taxonomy and catholicism."],
+    "W4":["SAUCE", "Usually served over and with other food."],
+    "W5":["TACO", "A Mexican sandwich."]
 };
 
 // Storing the central theme word
-export const Puzzle_ThemeWord = "LABOR";
+export const Puzzle_ThemeWord = "SALSA";
 
 // Storing the previous meso for streak purposes
-export const Previous_Meso = "HOOK";
+export const Previous_Meso = "LABOR";
 
 // Exporting the puzzle author
 export const Puzzle_Author = "Emil Karpinski";
