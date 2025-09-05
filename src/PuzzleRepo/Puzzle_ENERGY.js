530a374729667b4de8dd0a961d1773e3fcb16813
EmilKarpinski <emil.karpinski.mac@gmail.com>
2025-09-05 10:16:12 -0400

    Some reserve puzzles.

---
+++ src/PuzzleRepo/Puzzle_ENERGY.js
@@ -0,0 +1,18 @@
+// Storing the puzzle information (words and clues for export)
+export const Puzzle_Word_List = {
+    "W1":["FUEL", "e.g. gasoline or coal."], 
+    "W2":["GREEN", "Color of leaves or nature."],
+    "W3":["POWER", "Capability to produce an effect."],
+    "W4":["WORK", "Occupation or profession."],
+    "W5":["GAS", "State of matter or pumped at a station."],
+    "W6":["CAPACITY", "Maximum volume."],    
+};
+
+// Storing the central theme word
+export const Puzzle_ThemeWord = "ENERGY";
+
+// Storing the previous meso for streak purposes
+export const Previous_Meso = "";
+
+// Exporting the puzzle author
+export const Puzzle_Author = "Emil Karpinski";
--- src/PuzzleRepo/Puzzle_TIME.js
+++ src/PuzzleRepo/Puzzle_TIME.js
@@ -1,15 +1,13 @@
 // Storing the puzzle information (words and clues for export)
 export const Puzzle_Word_List = {
-    "W1":["", ""], 
-    "W2":["", ""],
-    "W3":["", ""],
-    "W4":["", ""],
-    "W5":["", ""],
-    "W6":["", ""],    
+    "W1":["MONTH", "Usually contain 30 or 31."], 
+    "W2":["MINUTE", "Just a ______."],
+    "W3":["MILLENIA", "We 'just' passed our second on the Gregorian calendar."],
+    "W4":["WEEK", "Seven days."]
 };
 
 // Storing the central theme word
-export const Puzzle_ThemeWord = "";
+export const Puzzle_ThemeWord = "TIME";
 
 // Storing the previous meso for streak purposes
 export const Previous_Meso = "";
