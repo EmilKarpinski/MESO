ce116f12293ad0e122edfa99205b1b8d50e8ff55
EmilKarpinski <emil.karpinski.mac@gmail.com>
2025-09-08 09:59:00 -0400

    2025-09-08

--- src/Puzzle.js
+++ src/Puzzle.js
@@ -1,16 +1,18 @@
 // Storing the puzzle information (words and clues for export)
 export const Puzzle_Word_List = {
-    "W1":["POWER", "Enery or influence."], 
-    "W2":["INTERNET", "Needed to access the online world."],
-    "W3":["TELEGRAM", "Electric note from the old days."],
-    "W4":["COPPER", "Reddish metal, great conductor."]
+    "W1":["FUEL", "e.g. gasoline or coal."], 
+    "W2":["GREEN", "Color of leaves or nature."],
+    "W3":["POWER", "Capability to produce an effect."],
+    "W4":["WORK", "What you do (occupation or profession)."],
+    "W5":["GAS", "State of matter or pumped at a station."],
+    "W6":["CAPACITY", "Maximum volume."],    
 };
 
 // Storing the central theme word
-export const Puzzle_ThemeWord = "WIRE";
+export const Puzzle_ThemeWord = "ENERGY";
 
 // Storing the previous meso for streak purposes
-export const Previous_Meso = "SQUARE";
+export const Previous_Meso = "WIRE";
 
 // Exporting the puzzle author
 export const Puzzle_Author = "Emil Karpinski";
--- src/PuzzleRepo/UsedPuzzles/Puzzle_ENERGY.js
+++ src/PuzzleRepo/UsedPuzzles/Puzzle_ENERGY.js
@@ -3,7 +3,7 @@ export const Puzzle_Word_List = {
     "W1":["FUEL", "e.g. gasoline or coal."], 
     "W2":["GREEN", "Color of leaves or nature."],
     "W3":["POWER", "Capability to produce an effect."],
-    "W4":["WORK", "Occupation or profession."],
+    "W4":["WORK", "What you do (occupation or profession)."],
     "W5":["GAS", "State of matter or pumped at a station."],
     "W6":["CAPACITY", "Maximum volume."],    
 };
@@ -12,7 +12,7 @@ export const Puzzle_Word_List = {
 export const Puzzle_ThemeWord = "ENERGY";
 
 // Storing the previous meso for streak purposes
-export const Previous_Meso = "";
+export const Previous_Meso = "WIRE";
 
 // Exporting the puzzle author
 export const Puzzle_Author = "Emil Karpinski";
