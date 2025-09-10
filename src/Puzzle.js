14c2bf35d816a3c41328ce18f56ae7627d5b4fd2
EmilKarpinski <emil.karpinski.mac@gmail.com>
2025-09-10 09:33:41 -0400

    2025-09-10

--- src/Puzzle.js
+++ src/Puzzle.js
@@ -1,16 +1,16 @@
 // Storing the puzzle information (words and clues for export)
 export const Puzzle_Word_List = {
-    "W1":["MONTH", "Usually contain 30 or 31."], 
-    "W2":["MINUTE", "Just a ______."],
-    "W3":["MILLENNIA", "We just finished two on the Gregorian calendar."],
-    "W4":["WEEK", "Seven days."]
+    "W1":["DEBT", "What you owe."], 
+    "W2":["RECEIPT", "Proof of payment."],
+    "W3":["DOLLAR", "Primarily North American legal tender."],
+    "W4":["TALLY", "A running count."]    
 };
 
 // Storing the central theme word
-export const Puzzle_ThemeWord = "TIME";
+export const Puzzle_ThemeWord = "BILL";
 
 // Storing the previous meso for streak purposes
-export const Previous_Meso = "ENERGY";
+export const Previous_Meso = "TIME";
 
 // Exporting the puzzle author
 export const Puzzle_Author = "Emil Karpinski";
--- src/PuzzleRepo/UsedPuzzles/Puzzle_BILL.js
+++ src/PuzzleRepo/UsedPuzzles/Puzzle_BILL.js
@@ -2,7 +2,7 @@
 export const Puzzle_Word_List = {
     "W1":["DEBT", "What you owe."], 
     "W2":["RECEIPT", "Proof of payment."],
-    "W3":["DOLLAR", "Legal tender."],
+    "W3":["DOLLAR", "Primarily North American legal tender."],
     "W4":["TALLY", "A running count."]    
 };
 
@@ -10,7 +10,7 @@ export const Puzzle_Word_List = {
 export const Puzzle_ThemeWord = "BILL";
 
 // Storing the previous meso for streak purposes
-export const Previous_Meso = "";
+export const Previous_Meso = "TIME";
 
 // Exporting the puzzle author
 export const Puzzle_Author = "Emil Karpinski";
