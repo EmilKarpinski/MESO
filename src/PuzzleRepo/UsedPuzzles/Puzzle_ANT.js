f6859181ed0735b49d53475eabba6c611dd585dc
EmilKarpinski <emil.karpinski.mac@gmail.com>
2025-09-16 09:18:54 -0400

    2025-09-16

--- src/PuzzleRepo/UsedPuzzles/Puzzle_ANT.js
+++ src/PuzzleRepo/UsedPuzzles/Puzzle_ANT.js
@@ -9,7 +9,7 @@ export const Puzzle_Word_List = {
 export const Puzzle_ThemeWord = "ANT";
 
 // Storing the previous meso for streak purposes
-export const Previous_Meso = "";
+export const Previous_Meso = "OIL";
 
 // Exporting the puzzle author
 export const Puzzle_Author = "Emil Karpinski";
