4c3fc917d29896b9d437563d659ceec39263fbe4
EmilKarpinski <emil.karpinski.mac@gmail.com>
2025-09-18 09:47:59 -0400

    2025-09-18

--- src/Puzzle.js
+++ src/Puzzle.js
@@ -1,16 +1,17 @@
 // Storing the puzzle information (words and clues for export)
 export const Puzzle_Word_List = {
-    "W1":["BOY", "Little dude."], 
-    "W2":["BIRTHDAY", "Once-a-year party."],
-    "W3":["BOTTLE", "Milk or beer container."],
-    "W4":["CRY", "Wail or sob."]    
+    "W1":["HERB", "Fresh or dried seasoning."], 
+    "W2":["GARNISH", "A little edible flourish."],
+    "W3":["PESTO", "Green pasta sauce."],
+    "W4":["PIZZA", "Pie of many regional takes."],
+    "W5":["ITALIAN", "Language spoken in Tuscany."]    
 };
 
 // Storing the central theme word
-export const Puzzle_ThemeWord = "BABY";
+export const Puzzle_ThemeWord = "BASIL";
 
 // Storing the previous meso for streak purposes
-export const Previous_Meso = "ANT";
+export const Previous_Meso = "BABY";
 
 // Exporting the puzzle author
 export const Puzzle_Author = "Emil Karpinski";
