6ef63b5d1ea4f1b511c78885f70b5c2bcb619459
EmilKarpinski <emil.karpinski.mac@gmail.com>
2025-09-22 09:15:16 -0400

    2025-09-22

--- src/Puzzle.js
+++ src/Puzzle.js
@@ -1,16 +1,16 @@
 // Storing the puzzle information (words and clues for export)
 export const Puzzle_Word_List = {
-    "W1":["SPICE", "Adds a little kick to a dish."], 
-    "W2":["HOT", "Warming."],
-    "W3":["TEA", "Hot leaf juice."],
-    "W4":["INDIA", "Country with Dehli-scious cuisine."]
+    "W1":["SPOT", "Marked by an X."], 
+    "W2":["KILLER", "Serial or silent type."],
+    "W3":["PACK", "Group that runs together or hunts together."],
+    "W4":["BLACK", "Formal color."]
 };
 
 // Storing the central theme word
-export const Puzzle_ThemeWord = "CHAI";
+export const Puzzle_ThemeWord = "ORCA";
 
 // Storing the previous meso for streak purposes
-export const Previous_Meso = "BASIL";
+export const Previous_Meso = "CHAI";
 
 // Exporting the puzzle author
 export const Puzzle_Author = "Emil Karpinski";
