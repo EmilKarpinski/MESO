db0a59fcfcede4470776dabe5e495db15cc290b4
EmilKarpinski <emil.karpinski.mac@gmail.com>
2025-09-24 09:31:02 -0400

    2025-09-24

--- src/Puzzle.js
+++ src/Puzzle.js
@@ -1,16 +1,16 @@
 // Storing the puzzle information (words and clues for export)
 export const Puzzle_Word_List = {
-    "W1":["MAMMAL", "Hairy warm-blooded vertebrate."], 
-    "W2":["WHITE", "Like sugar, snow, and surrender."],
-    "W3":["LIQUID", "Fills and pours."],
-    "W4":["DRINK", "Might acommpany a toast."]
+    "W1":["ANIMAL", "Critter."], 
+    "W2":["BURROW", "To dig and go around ground."],
+    "W3":["BLIND", "Can't or won't see."],
+    "W4":["TUNNEL", "Subterran hallway."]
 };
 
 // Storing the central theme word
-export const Puzzle_ThemeWord = "MILK";
+export const Puzzle_ThemeWord = "MOLE";
 
 // Storing the previous meso for streak purposes
-export const Previous_Meso = "ORCA";
+export const Previous_Meso = "MILK";
 
 // Exporting the puzzle author
 export const Puzzle_Author = "Emil Karpinski";
