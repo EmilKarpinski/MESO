01f38b6e79b55e0056962fd8f82f0884a4959574
EmilKarpinski <emil.karpinski.mac@gmail.com>
2025-09-30 09:21:10 -0400

    2025-09-30

--- src/Puzzle.js
+++ src/Puzzle.js
@@ -1,17 +1,16 @@
 // Storing the puzzle information (words and clues for export)
 export const Puzzle_Word_List = {
-    "W1":["PEDAL", "Speed foot contorller."], 
-    "W2":["WHITE", "Moves first on a chessboard."],
-    "W3":["BLACK", "Often paried against the above word."],
-    "W4":["GRAND", "Vast; amazing."],
-    "W5":["KEYBOARD", "Clacks when you type."]
+    "W1":["COWBOY", "Wrangler or ranger."], 
+    "W2":["TAURUS", "Early spring star sign."],
+    "W3":["MALE", "He, not she."],
+    "W4":["CATTLE", "Mooo-ved by ranchers."]
 };
 
 // Storing the central theme word
-export const Puzzle_ThemeWord = "PIANO";
+export const Puzzle_ThemeWord = "BULL";
 
 // Storing the previous meso for streak purposes
-export const Previous_Meso = "NEWS";
+export const Previous_Meso = "PIANO";
 
 // Exporting the puzzle author
 export const Puzzle_Author = "Emil Karpinski";
