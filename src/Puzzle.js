0ae81d54ccf4bbb9d2d9ae7da20d082e034e24c0
EmilKarpinski <emil.karpinski.mac@gmail.com>
2025-10-02 09:09:49 -0400

    2025-10-02

--- src/Puzzle.js
+++ src/Puzzle.js
@@ -1,17 +1,17 @@
 // Storing the puzzle information (words and clues for export)
 export const Puzzle_Word_List = {
-    "W1":["MEASURE", "Determine length or volume?"], 
-    "W2":["KITCHEN", "Pot and pan place."],
-    "W3":["BATHROOM", "Toilet territory."],
-    "W4":["BALANCE", "aka poise."],
-    "W5":["WEIGHT", "Mass or heft."]
+    "W1":["MYSTIC", "Spiritual seer."], 
+    "W2":["WIZARD", "e.g. Gandalf."],
+    "W3":["SLEIGHT", "Deception or insult."],
+    "W4":["COIN", "Clinky cash."],
+    "W5":["TRICK", "Playful deception."]
 };
 
 // Storing the central theme word
-export const Puzzle_ThemeWord = "SCALE";
+export const Puzzle_ThemeWord = "MAGIC";
 
 // Storing the previous meso for streak purposes
-export const Previous_Meso = "BULL";
+export const Previous_Meso = "SCALE";
 
 // Exporting the puzzle author
 export const Puzzle_Author = "Emil Karpinski";
