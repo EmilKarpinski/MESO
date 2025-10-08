cf36ae6619cebccc265a3830637b9f9153ec5e74
EmilKarpinski <emil.karpinski.mac@gmail.com>
2025-10-08 09:54:37 -0400

    2025-10-08

--- src/Puzzle.js
+++ src/Puzzle.js
@@ -1,16 +1,16 @@
 // Storing the puzzle information (words and clues for export)
 export const Puzzle_Word_List = {
-    "W1":["DOZEN", "Twelve."], 
-    "W2":["COIN", "Metal token."],
-    "W3":["MONEY", "aka cash."],
-    "W4":["TENTH", "10%."]
+    "W1":["MENU", "List of edibles."], 
+    "W2":["DINNER", "Evening repast."],
+    "W3":["BREAKFAST", "Two are enjoyed by hungry hobbits."],
+    "W4":["LUNCH", "Midday break."]
 };
 
 // Storing the central theme word
-export const Puzzle_ThemeWord = "DIME";
+export const Puzzle_ThemeWord = "MEAL";
 
 // Storing the previous meso for streak purposes
-export const Previous_Meso = "ICE";
+export const Previous_Meso = "DIME";
 
 // Exporting the puzzle author
 export const Puzzle_Author = "Emil Karpinski";
