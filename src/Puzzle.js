9488781983b604b7b5a3129ebacb40c1a3028e55
Emil Karpinski <102920028+EmilKarpinski@users.noreply.github.com>
2025-10-09 11:19:29 -0400

    2025-10-09

--- src/Puzzle.js
+++ src/Puzzle.js
@@ -1,16 +1,21 @@
 // Storing the puzzle information (words and clues for export)
 export const Puzzle_Word_List = {
-    "W1":["MENU", "List of edibles."], 
-    "W2":["DINNER", "Evening repast."],
-    "W3":["BREAKFAST", "Two are enjoyed by hungry hobbits."],
-    "W4":["LUNCH", "Midday break."]
+    "W1":["QUESTION", "Indicated by a raised hand."], 
+    "W2":["PANEL", "Opinionated group."],
+    "W3":["EXPERT", "Master of a subject."],
+    "W4":["QUOTE", "Contains specific words."],
+    "W5":["PRESS", "News force or to apply pressure."],
+    "W6":["VIDEO", "Watched not read."],
+    "W7":["HIRE", "Bring on board."],
+    "W8":["ANSWER", "aka response."],
+    "W9":["NEWS", "Breaking or more recently fake."]
 };
 
 // Storing the central theme word
-export const Puzzle_ThemeWord = "MEAL";
+export const Puzzle_ThemeWord = "INTERVIEW";
 
 // Storing the previous meso for streak purposes
-export const Previous_Meso = "DIME";
+export const Previous_Meso = "MEAL";
 
 // Exporting the puzzle author
 export const Puzzle_Author = "Emil Karpinski";
