73945208011e8d2977a4a84963f27d69fcb2edc2
Emil Karpinski <102920028+EmilKarpinski@users.noreply.github.com>
2025-10-13 08:55:31 -0400

    Monday commit

--- src/Puzzle.js
+++ src/Puzzle.js
@@ -1,18 +1,17 @@
 // Storing the puzzle information (words and clues for export)
 export const Puzzle_Word_List = {
-    "W1":["BIRD", "Nester."], 
-    "W2":["ISLAND", "Surrounded territory."],
-    "W3":["NATURAL", "aka organic or wild."],
-    "W4":["LAW", "Authority and order."],
-    "W5":["FINCH", "Small-beaked singer."],
-    "W6":["EVOLUTION", "Pokemon progression."],
+    "W1":["VOLUME", "Book series part."], 
+    "W2":["FILM", "Thin Hollywood layer."],
+    "W3":["MEDIA", "e.g. Radio and TV."],
+    "W4":["PAUSE", "Brief break."],
+    "W5":["STOP", "Cease."]
 };
 
 // Storing the central theme word
-export const Puzzle_ThemeWord = "DARWIN";
+export const Puzzle_ThemeWord = "VIDEO";
 
 // Storing the previous meso for streak purposes
-export const Previous_Meso = "INTERVIEW";
+export const Previous_Meso = "DARWIN";
 
 // Exporting the puzzle author
 export const Puzzle_Author = "Emil Karpinski";
