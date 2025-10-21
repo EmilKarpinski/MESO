c4b6be6e6f04e209d97c69b2d2ec0cfa27d2b7c2
EmilKarpinski <emil.karpinski.mac@gmail.com>
2025-10-21 09:23:38 -0400

    2025-10-21

--- src/Puzzle.js
+++ src/Puzzle.js
@@ -1,16 +1,16 @@
 // Storing the puzzle information (words and clues for export)
 export const Puzzle_Word_List = {
-    "W1":["DORM", "Short form college sleeping location."], 
-    "W2":["HOUSE", "Stand-alone aspiration."],
-    "W3":["DOOR", "An open or closed portal."],
-    "W4":["CHAMBER", "Fancy location for formal or otherwise large gatherings."]
+    "W1":["SHAPES", "Geometery's focus."], 
+    "W2":["SQUARE", "All the same all around."],
+    "W3":["LENGTH", "From here to there."],
+    "W4":["MATH", "-,X,+?"]
 };
 
 // Storing the central theme word
-export const Puzzle_ThemeWord = "ROOM";
+export const Puzzle_ThemeWord = "AREA";
 
 // Storing the previous meso for streak purposes
-export const Previous_Meso = "GASTON";
+export const Previous_Meso = "ROOM";
 
 // Exporting the puzzle author
 export const Puzzle_Author = "Emil Karpinski";
--- src/PuzzleRepo/UsedPuzzles/Puzzle_AREA.js
+++ src/PuzzleRepo/UsedPuzzles/Puzzle_AREA.js
@@ -1,6 +1,6 @@
 // Storing the puzzle information (words and clues for export)
 export const Puzzle_Word_List = {
-    "W1":["SHAPE", "Geometery's subject."], 
+    "W1":["SHAPES", "Geometery's focus."], 
     "W2":["SQUARE", "All the same all around."],
     "W3":["LENGTH", "From here to there."],
     "W4":["MATH", "-,X,+?"]
@@ -10,7 +10,7 @@ export const Puzzle_Word_List = {
 export const Puzzle_ThemeWord = "AREA";
 
 // Storing the previous meso for streak purposes
-export const Previous_Meso = "";
+export const Previous_Meso = "ROOM";
 
 // Exporting the puzzle author
 export const Puzzle_Author = "Emil Karpinski";
