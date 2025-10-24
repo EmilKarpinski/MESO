21dfd82919af48d17d4b4c7268219cc6cbe3f736
Emil Karpinski <102920028+EmilKarpinski@users.noreply.github.com>
2025-10-24 10:09:40 -0400

    Update Puzzle.js

--- src/Puzzle.js
+++ src/Puzzle.js
@@ -1,16 +1,16 @@
 // Storing the puzzle information (words and clues for export)
 export const Puzzle_Word_List = {
-    "W1":["SHAPES", "Geometery's focus."], 
-    "W2":["SQUARE", "All the same all around."],
-    "W3":["LENGTH", "From here to there."],
-    "W4":["MATH", "-,X,+?"]
+    "W1":["COUNCIL", "Local governing group."], 
+    "W2":["BUILDINGS", "Skyline structures."],
+    "W3":["STREETS", "Paved paths."],
+    "W4":["MAYOR", "Local leader."]
 };
 
 // Storing the central theme word
-export const Puzzle_ThemeWord = "AREA";
+export const Puzzle_ThemeWord = "CITY";
 
 // Storing the previous meso for streak purposes
-export const Previous_Meso = "ROOM";
+export const Previous_Meso = "AREA";
 
 // Exporting the puzzle author
 export const Puzzle_Author = "Emil Karpinski";
