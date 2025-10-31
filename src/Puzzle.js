dd1253474893a91ab1cae9e4769bde2732565fd6
EmilKarpinski <emil.karpinski.mac@gmail.com>
2025-10-31 09:45:31 -0400

    2025-10-31

--- src/Puzzle.js
+++ src/Puzzle.js
@@ -1,16 +1,16 @@
 // Storing the puzzle information (words and clues for export)
 export const Puzzle_Word_List = {
-    "W1":["BOTTOM", "Lowest point."], 
-    "W2":["ARMY", "Rank and file."],
-    "W3":["POST", "Stick a notice or deliver it."],
-    "W4":["PLATE", "Dinner dish."]
+    "W1":["PRESS", "Media collective."], 
+    "W2":["BULL", "A cow that would be difficult to get milk from."],
+    "W3":["SHOVE", "Move aside roughly."],
+    "W4":["HIT", "Chart-topper."]
 };
 
 // Storing the central theme word
-export const Puzzle_ThemeWord = "BASE";
+export const Puzzle_ThemeWord = "PUSH";
 
 // Storing the previous meso for streak purposes
-export const Previous_Meso = "END";
+export const Previous_Meso = "BASE";
 
 // Exporting the puzzle author
 export const Puzzle_Author = "Emil Karpinski";
