cfb510551755eea5186d617235627789c0e09afe
EmilKarpinski <emil.karpinski.mac@gmail.com>
2025-11-07 11:56:51 -0500

    2025-11-07

--- src/Puzzle.js
+++ src/Puzzle.js
@@ -1,17 +1,17 @@
 // Storing the puzzle information (words and clues for export)
 export const Puzzle_Word_List = {
-    "W1":["FINGER", "Handy appendage."], 
-    "W2":["BLOW", "Deliver a punch or ____."],
-    "W3":["MUSIC", "Heard harmony."],
-    "W4":["SPIT", "Emit words or fluids."],
-    "W5":["PIPE", "Small tube."]
+    "W1":["DRIVE", "Operate a car or boat."], 
+    "W2":["REMOVE", "Take away."],
+    "W3":["STOP", "'Freeze!'"],
+    "W4":["COUNTER", "Oppose or respond in kind."],
+    "W5":["BLOCK", "Obstruct passage."]
 };
 
 // Storing the central theme word
-export const Puzzle_ThemeWord = "FLUTE";
+export const Puzzle_ThemeWord = "REPEL";
 
 // Storing the previous meso for streak purposes
-export const Previous_Meso = "TALK";
+export const Previous_Meso = "FLUTE";
 
 // Exporting the puzzle author
 export const Puzzle_Author = "Emil Karpinski";
