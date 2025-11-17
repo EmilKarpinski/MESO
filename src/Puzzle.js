7f35615cd0ae2438399de991bedf3490cdfeeabe
EmilKarpinski <emil.karpinski.mac@gmail.com>
2025-11-17 09:11:26 -0500

    2025-11-17

--- src/Puzzle.js
+++ src/Puzzle.js
@@ -1,15 +1,16 @@
 // Storing the puzzle information (words and clues for export)
 export const Puzzle_Word_List = {
-    "W1":["PUBLIC", "Open to all."], 
-    "W2":["ROUTE", "Path to a destination."],
-    "W3":["STATION", "Start, stop, or waypoint structure."]
+    "W1":["STOP", "'Halt!'"], 
+    "W2":["YIELD", "Give way."],
+    "W3":["MERGE", "Combine companies or lanes."],
+    "W4":["TRAIN", "Teach a skill or catch a ride."]
 };
 
 // Storing the central theme word
-export const Puzzle_ThemeWord = "BUS";
+export const Puzzle_ThemeWord = "SIGN";
 
 // Storing the previous meso for streak purposes
-export const Previous_Meso = "SUGAR";
+export const Previous_Meso = "BUS";
 
 // Exporting the puzzle author
 export const Puzzle_Author = "Emil Karpinski";
