44b2ba5cb79c1438f96aec4ce23117fbd7ad585c
EmilKarpinski <emil.karpinski.mac@gmail.com>
2025-11-25 09:40:03 -0500

    2025-11-25

--- src/Puzzle.js
+++ src/Puzzle.js
@@ -1,16 +1,18 @@
 // Storing the puzzle information (words and clues for export)
 export const Puzzle_Word_List = {
-    "W1":["IVY", "Poisonous supervillian."], 
-    "W2":["TRELLIS", "Lattice for garden climbers."],
-    "W3":["PLANT", "Flora not fauana."],
-    "W4":["GRAPE", "Wine precursor."]
+    "W1":["WICK", "Lit part of dynamite."], 
+    "W2":["WAX", "Polish or preserve."],
+    "W3":["LANTERN", "Old-fashioned flashlight."],
+    "W4":["HOLDER", "Frame or base that secures something."],
+    "W5":["LIGHT", "Opposite of heavy."],
+    "W6":["CAKE", "Sliced circular treat."]
 };
 
 // Storing the central theme word
-export const Puzzle_ThemeWord = "VINE";
+export const Puzzle_ThemeWord = "CANDLE";
 
 // Storing the previous meso for streak purposes
-export const Previous_Meso = "GIVE";
+export const Previous_Meso = "VINE";
 
 // Exporting the puzzle author
 export const Puzzle_Author = "Emil Karpinski";
