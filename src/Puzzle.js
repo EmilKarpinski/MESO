7b54d6772076ac8a9244421c79d2cd53254f9a0a
EmilKarpinski <emil.karpinski.mac@gmail.com>
2025-12-01 10:24:47 -0500

    2025-12-01

--- src/Puzzle.js
+++ src/Puzzle.js
@@ -1,17 +1,18 @@
 // Storing the puzzle information (words and clues for export)
 export const Puzzle_Word_List = {
-    "W1":["BROWN", "Earthy color."], 
-    "W2":["STURDY", "Solid; built to last."],
-    "W3":["BUILDING", "A structure."],
-    "W4":["RECTANGLE", "Unevern four corners."],
-    "W5":["KILN", "Hot oven for clay or glass."]
+    "W1":["THIGH", "Upper leg portion."], 
+    "W2":["STUFFED", "Too full."],
+    "W3":["ROAST", "Oven-cooked."],
+    "W4":["DRUMSTICK", "Piece with a handle."],
+    "W5":["MEAL", "Described by a menu."],
+    "W6":["POULTRY", "Edible feathered friends."]
 };
 
 // Storing the central theme word
-export const Puzzle_ThemeWord = "BRICK";
+export const Puzzle_ThemeWord = "TURKEY";
 
 // Storing the previous meso for streak purposes
-export const Previous_Meso = "CANDLE";
+export const Previous_Meso = "BRICK";
 
 // Exporting the puzzle author
 export const Puzzle_Author = "Emil Karpinski";
