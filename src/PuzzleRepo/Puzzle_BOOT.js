3284241bc030888792e68c2449a755d561a259ec
EmilKarpinski <emil.karpinski.mac@gmail.com>
2025-12-03 15:26:58 -0500

    Puzzle for tomorrow

---
+++ src/PuzzleRepo/Puzzle_BOOT.js
@@ -0,0 +1,16 @@
+// Storing the puzzle information (words and clues for export)
+export const Puzzle_Word_List = {
+    "W1":["COWBOY", "Half man; half steer."], 
+    "W2":["SHOE", "Lowest fashion."],
+    "W3":["TOE", "Stubbed or iron."],
+    "W4":["LEATHER", "Thoroughly tanned."]
+};
+
+// Storing the central theme word
+export const Puzzle_ThemeWord = "BOOT";
+
+// Storing the previous meso for streak purposes
+export const Previous_Meso = "ICE";
+
+// Exporting the puzzle author
+export const Puzzle_Author = "Emil Karpinski";
