cacc42ce688b3908e013afe8362fff54a9437a9f
EmilKarpinski <emil.karpinski.mac@gmail.com>
2025-12-04 09:50:39 -0500

    2025-12-04

--- src/Puzzle.js
+++ src/Puzzle.js
@@ -1,15 +1,16 @@
 // Storing the puzzle information (words and clues for export)
 export const Puzzle_Word_List = {
-    "W1":["SOLID", "Sturdy, reliable."], 
-    "W2":["BLACK", "Or white."],
-    "W3":["SLIPPERY", "Difficult to catch or stand on."]
+    "W1":["COWBOY", "Half man; half steer."], 
+    "W2":["SHOE", "Lowest fashion."],
+    "W3":["TOE", "Stubbed or iron."],
+    "W4":["LEATHER", "Thoroughly tanned."]
 };
 
 // Storing the central theme word
-export const Puzzle_ThemeWord = "ICE";
+export const Puzzle_ThemeWord = "BOOT";
 
 // Storing the previous meso for streak purposes
-export const Previous_Meso = "SEED";
+export const Previous_Meso = "ICE";
 
 // Exporting the puzzle author
 export const Puzzle_Author = "Emil Karpinski";
