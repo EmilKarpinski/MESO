814b13841bae2af0aa35c221654f5c282846c618
EmilKarpinski <emil.karpinski.mac@gmail.com>
2025-12-09 09:26:27 -0500

    2025-12-09

--- src/Puzzle.js
+++ src/Puzzle.js
@@ -1,16 +1,16 @@
 // Storing the puzzle information (words and clues for export)
 export const Puzzle_Word_List = {
-    "W1":["CALF", "A young spotted baby."], 
-    "W2":["SHANK", "Street-wise or improvised stab."],
-    "W3":["MUTTON", "Sheep meat."],
-    "W4":["BUTCHER", "Preparer of fine meats."]
+    "W1":["MAN", "Fellow or chap."], 
+    "W2":["PACKAGE", "Wrapped or boxed."],
+    "W3":["BILL", "Expense or money owed."],
+    "W4":["LETTER", "A, B, or C."]
 };
 
 // Storing the central theme word
-export const Puzzle_ThemeWord = "LAMB";
+export const Puzzle_ThemeWord = "MAIL";
 
 // Storing the previous meso for streak purposes
-export const Previous_Meso = "BOOT";
+export const Previous_Meso = "LAMB";
 
 // Exporting the puzzle author
 export const Puzzle_Author = "Emil Karpinski";
