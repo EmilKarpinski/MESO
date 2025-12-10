85f262e754bc69f1189fd86f104eda9bd509c0bf
EmilKarpinski <emil.karpinski.mac@gmail.com>
2025-12-10 09:32:31 -0500

    2025-12-10

--- src/Puzzle.js
+++ src/Puzzle.js
@@ -1,16 +1,16 @@
 // Storing the puzzle information (words and clues for export)
 export const Puzzle_Word_List = {
-    "W1":["MAN", "Fellow or chap."], 
-    "W2":["PACKAGE", "Wrapped or boxed."],
-    "W3":["BILL", "Expense or money owed."],
-    "W4":["LETTER", "A, B, or C."]
+    "W1":["NAIL", "Hammered hardware."], 
+    "W2":["RUST", "Corrosion."],
+    "W3":["ALLOY", "Metal mix."],
+    "W4":["PAN", "Skillet or flutist."]
 };
 
 // Storing the central theme word
-export const Puzzle_ThemeWord = "MAIL";
+export const Puzzle_ThemeWord = "IRON";
 
 // Storing the previous meso for streak purposes
-export const Previous_Meso = "LAMB";
+export const Previous_Meso = "MAIL";
 
 // Exporting the puzzle author
 export const Puzzle_Author = "Emil Karpinski";
--- src/PuzzleRepo/UsedPuzzles/Puzzle_IRON.js
+++ src/PuzzleRepo/UsedPuzzles/Puzzle_IRON.js
@@ -10,7 +10,7 @@ export const Puzzle_Word_List = {
 export const Puzzle_ThemeWord = "IRON";
 
 // Storing the previous meso for streak purposes
-export const Previous_Meso = "";
+export const Previous_Meso = "MAIL";
 
 // Exporting the puzzle author
 export const Puzzle_Author = "Emil Karpinski";
