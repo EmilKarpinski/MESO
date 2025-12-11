6d0f6c6106a7300811ce0e438b2c8e0eb65e0465
EmilKarpinski <emil.karpinski.mac@gmail.com>
2025-12-11 09:47:19 -0500

    2025-12-11

--- src/Puzzle.js
+++ src/Puzzle.js
@@ -1,16 +1,17 @@
 // Storing the puzzle information (words and clues for export)
 export const Puzzle_Word_List = {
-    "W1":["NAIL", "Hammered hardware."], 
-    "W2":["RUST", "Corrosion."],
-    "W3":["ALLOY", "Metal mix."],
-    "W4":["PAN", "Skillet or flutist."]
+    "W1":["STRANGE", "Unusual; out of the ordinary."], 
+    "W2":["ANOMALY", "Irregular occurance."],
+    "W3":["EXOTIC", "Tiger King's Joe."],
+    "W4":["WEIRD", "Intriguingly odd."],
+    "W5":["FOREIGN", "Unfamiliar or imported."]
 };
 
 // Storing the central theme word
-export const Puzzle_ThemeWord = "IRON";
+export const Puzzle_ThemeWord = "ALIEN";
 
 // Storing the previous meso for streak purposes
-export const Previous_Meso = "MAIL";
+export const Previous_Meso = "IRON";
 
 // Exporting the puzzle author
 export const Puzzle_Author = "Emil Karpinski";
