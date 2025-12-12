6c60ac761e073173b3cc3d85f30452f3d36ad1d8
EmilKarpinski <emil.karpinski.mac@gmail.com>
2025-12-12 09:47:51 -0500

    2025-12-12

--- src/PuzzleRepo/UsedPuzzles/Puzzle_CONSUME.js
+++ src/PuzzleRepo/UsedPuzzles/Puzzle_CONSUME.js
@@ -13,7 +13,7 @@ export const Puzzle_Word_List = {
 export const Puzzle_ThemeWord = "CONSUME";
 
 // Storing the previous meso for streak purposes
-export const Previous_Meso = "TURKEY";
+export const Previous_Meso = "ALIEN";
 
 // Exporting the puzzle author
 export const Puzzle_Author = "Emil Karpinski";
