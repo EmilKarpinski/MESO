65c2deaf69facf3ab1fddc995bef1f8066259abc
EmilKarpinski <emil.karpinski.mac@gmail.com>
2025-12-20 09:35:25 -0500

    2025-12-20

--- src/Puzzle.js
+++ src/Puzzle.js
@@ -1,15 +1,18 @@
 // Storing the puzzle information (words and clues for export)
 export const Puzzle_Word_List = {
-    "W1":["WARNING", "Caution."], 
-    "W2":["REINDEER", "Animal with built in lantern."],
-    "W3":["CARD", "Deck component."]
+    "W1":["IMAGE", "Physical likeness."], 
+    "W2":["VANITY", "Excessive pride."],
+    "W3":["REFLECT", "Throw back."],
+    "W4":["PORTRAY", "Depict or act."],
+    "W5":["DISTORT", "Depict or act incorrectly."],
+    "W6":["BATHROOM", "Towels and toiletries location."]
 };
 
 // Storing the central theme word
-export const Puzzle_ThemeWord = "RED";
+export const Puzzle_ThemeWord = "MIRROR";
 
 // Storing the previous meso for streak purposes
-export const Previous_Meso = "TEST";
+export const Previous_Meso = "RED";
 
 // Exporting the puzzle author
 export const Puzzle_Author = "Emil Karpinski";
